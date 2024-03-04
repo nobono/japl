3a0d4606c3dcafb11634a696ce1f7dd4f82cc20a
david <shincdavid@gmail.com>
2024-03-04 15:21:21 -0500

    n/a

--- autopilot.py
+++ autopilot.py
@@ -17,7 +17,7 @@ if __name__ == "__main__":
     t = np.linspace(0, 10, 1000)
     x0 = np.array([0, 0])
     U = [np.sin(i) for i in t]
-    sol = ct.forced_response(ss, t, U, x0, return_x=True)
+    sol = ct.forced_response(ss, t, U, x0, return_x=True) #type:ignore
     tt = sol[0]
     y = sol[1]
     s = sol[2].T
