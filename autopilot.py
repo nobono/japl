ce1419a955ab53f2cf9df820194e12f281d2ea85
david <shincdavid@gmail.com>
2024-03-05 01:28:56 -0500

    n/a

--- autopilot.py
+++ autopilot.py
@@ -8,16 +8,18 @@ import numpy as np
 
 
 wapar = 25
-zetapar = .2
+zetapar = .1
 ss = ct.tf2ss([1], [1/wapar**2, 2*zetapar/wapar, 1])
 ss, TM = ct.observable_form(ss)
 
 
 if __name__ == "__main__":
+    ss_model = ss
     t = np.linspace(0, 10, 1000)
-    x0 = np.array([0, 0])
+    x0 = np.array([0])
     U = [np.sin(i) for i in t]
-    sol = ct.forced_response(ss, t, U, x0, return_x=True) #type:ignore
+    # U = [1] * 1000
+    sol = ct.forced_response(ss_model, t, U, x0, return_x=True) #type:ignore
     tt = sol[0]
     y = sol[1]
     s = sol[2].T
--- main.py
+++ main.py
@@ -3,6 +3,7 @@ import control as ct
 import matplotlib.pyplot as plt
 import scipy
 from scipy import constants
+from scipy.linalg import norm
 from scipy.integrate import solve_ivp
 from scipy.integrate._ivp.base import OdeSolver
 from autopilot import ss as apss
@@ -52,7 +53,8 @@ ss = ct.ss(A, B, C, D)
 
 
 def unitize(vec):
-    return vec / scipy.linalg.norm(vec)
+    vec = np.asarray(vec)
+    return vec / norm(vec)
 
 
 def create_Rt(xx, yy, zz):
@@ -72,17 +74,54 @@ def weave_maneuver(t, X):
     return Rt @ ac
 
 
-def popup_maneuver(t, X):
-    vm = unitize(X[3:6])
-    base2 = [1, 0, 0]
-    Rt = create_Rt(
-            vm,
-            np.cross(vm, base2),
-            base2
-            )
-    ac = np.zeros((3,))
-    ac[1] = -1*np.sin(.3 * t)
-    return Rt @ ac
+gd_phase = 0
+
+def popup_maneuver(t, X, r_targ, ac):
+    # vm = unitize(X[3:6])
+    # base2 = [1, 0, 0]
+    # Rt = create_Rt(
+    #         vm,
+    #         np.cross(vm, base2),
+    #         base2
+    #         )
+    # ac = np.zeros((3,))
+    # ac[1] = -1*np.sin(.3 * t)
+    # return Rt @ ac
+    START_POP_RANGE = 6.5e3
+    STOP_POP_ALT = 90
+    START_DIVE_RANGE = 8e3
+    STOP_DIVE_ALT = 30 # 60
+    rm = X[:3]
+    vm = X[3:6]
+    global gd_phase
+    match gd_phase:
+        case 0 :
+            r_pop = np.array([0, START_POP_RANGE, 90])
+            ac = ac + pronav(X, r_pop, np.array([0, 0, 0]), N=4)
+            if rm[2] >= STOP_POP_ALT:
+                gd_phase += 1
+        case 1 :
+            r_pop = np.array([0, START_DIVE_RANGE, 10])
+            ac = ac + pronav(X, r_pop, np.array([0, 0, 0]), N=3)
+            if rm[2] <= STOP_DIVE_ALT:
+                gd_phase += 1
+        case 2 :
+            # Kp = 80.0
+            # Kp_rz = 0.0006
+            # rz_err = Kp_rz * (max(min(10 - rm[2], 10), -10) / 10)
+            # vmd_hat = unitize([0, 1, rz_err])
+            # vm_hat = unitize(vm)
+            # vm_err = vmd_hat - vm_hat
+            # ac = ac + Kp * vm_err
+            r_pop = np.array([0, 13e3, 10])
+            ac = ac + pronav(X, r_pop, np.array([0, 0, 0]), N=40)
+            if rm[1] > 12e3:
+                gd_phase += 1
+        case 3 :
+            ac = ac + pronav(X, r_targ, np.array([0, 0, 0]), N=4)
+        case _ :
+            pass
+    return ac
 
 
 def pronav(X, r_targ, v_targ, N=4.0):
@@ -95,44 +134,52 @@ def pronav(X, r_targ, v_targ, N=4.0):
     return ac
 
 
+def atmosphere_model(t, X):
+    rm = X[:3]
+    vm = X[3:6]
+    ATMOS_BOUNDS = [-5e3, 81e3]
+    atmos = Atmosphere(min(max((rm[2] / 1000.0), ATMOS_BOUNDS[0]), ATMOS_BOUNDS[1]))
+    CD = 0.45
+    A = .25**2
+    xfd = -(0.5 * CD * A * atmos.density[0] * vm[0])
+    yfd = -(0.5 * CD * A * atmos.density[0] * vm[1])
+    zfd = -(0.5 * CD * A * atmos.density[0] * vm[2])
+    return np.array([xfd, yfd, zfd])
+
+
 def guidance(t, X, r_targ):
     GLIMIT = 14.0
     rm = X[:3]
-    ac = np.array([0, 0, 0])
+    vm = X[3:6]
+    ac = unitize(vm) * 3.5
     # ac = weave_maneuver(t, X)
     # ac_man = popup_maneuver(t, X)
-    if 5e3 < rm[1] <= 10e3:
-        if 0 < rm[2] <= 200:
-            r_pop = np.array([0, 7e3, 200])
-            ac = ac + pronav(X, r_pop, np.array([0, 0, 0]), N=3)
-        if 7e3 < rm[1] <= 10e3:
-            r_pop = np.array([0, 10e3, 10])
-            ac = ac + pronav(X, r_pop, np.array([0, 0, 0]), N=6)
-        # elif 10e3 < rm[1]:
-        #     ac = ac + pronav(X, r_targ, np.array([0, 0, 0]))
-    if scipy.linalg.norm(ac) > GLIMIT:
+
+    if 5e3 < rm[1]:
+        ac = popup_maneuver(t, X, r_targ, ac)
+
+    if norm(ac) > GLIMIT:
         ac = unitize(ac) * GLIMIT
     return ac
 
 
 def dynamics(t, X, ss, r_targ):
     ac = guidance(t, X, r_targ)
-    ac = [0, 5030, 0]
-    CD = 0.4
-    U = np.array([-(CD*X[3]**2), -(CD*X[4]**2), -(CD*X[5]**2),
-                  *ac])
+    # ac = [0, 3, 0]
+    fd = atmosphere_model(t, X)
+    U = np.array([*fd, *ac])
     Xdot = ss.A @ X + ss.B @ U
     return Xdot
 
 
 # Inits
 ####################################
-t_span = [0, 300]
+t_span = [0, 200]
 x0 = np.zeros((12))
 x0[:3] = np.array([0, 0, 10])    #R0
 x0[3:6] = np.array([0, 200, 0])   #V0
 
-targ_R0 = np.array([0, 20e3, 0])
+targ_R0 = np.array([0, 50e3, 0])
 ####################################
 
 
@@ -180,14 +227,12 @@ y = sol['y'].T
 # ax3.plot(t, y[:, 5])
 # ax3.set_title("ac")
 
-r_pop1 = np.array([0, 7e3, 300])
-r_pop2 = np.array([0, 10e3, 10])
+r_pop1 = np.array([0, 7e3, 90])
+r_pop2 = np.array([0, 9e3, 10])
 
 # 3D Plot
-# fig, (ax, ax2)= plt.subplots(2, figsize=(10, 8))
-fig = plt.figure(figsize=(15, 10))
+fig = plt.figure(figsize=(10, 8))
 ax = plt.axes(projection='3d')
-# ax = fig.add_subplot(2, 1, 1, projection='3d')
 ax.plot3D(y[:, 0], y[:, 1], y[:, 2])
 ax.plot3D(*targ_R0, marker='.')
 ax.plot3D(*r_pop1, marker='.', color='green')
@@ -197,9 +242,8 @@ ax.set_ylabel("N")
 ax.set_zlabel("D")
 
 fig2, (ax2, ax3, ax4) = plt.subplots(3, figsize=(10, 8))
-# ax2.plot(y[:, 0], y[:, 1])
-# ax2.set_title("xy")
-
+ax2.plot(y[:, 1], y[:, 2])
+ax2.set_title("y")
 ax3.plot(y[:, 1], y[:, 4])
 ax3.set_title("yvel")
 ax4.plot(y[:, 1], y[:, 5])
