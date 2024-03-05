4486639c12caba0a72ac295cbdd02d93f613ef94
david.shin <david.shin@jhuapl.edu>
2024-03-05 15:53:25 -0500

    n/a

--- main.py
+++ main.py
@@ -8,7 +8,6 @@ sys.path.append(lib_path)
 
 import argparse
 import numpy as np
-import control as ct
 import matplotlib.pyplot as plt
 
 # ---------------------------------------------------
@@ -24,7 +23,6 @@ from util import bound
 
 # ---------------------------------------------------
 
-from autopilot import ss as apss
 from atmosphere import Atmosphere
 
 # ---------------------------------------------------
@@ -34,52 +32,15 @@ from maneuvers import Maneuvers
 
 # ---------------------------------------------------
 
+from dynamics import ss
+
+# ---------------------------------------------------
+
 
 
 atmosphere = Atmosphere()
 maneuvers = Maneuvers()
 
-
-
-A = np.array([
-    [0, 0, 0,   1, 0, 0,    0, 0, 0,    0, 0, 0], # xvel
-    [0, 0, 0,   0, 1, 0,    0, 0, 0,    0, 0, 0], # yvel
-    [0, 0, 0,   0, 0, 1,    0, 0, 0,    0, 0, 0], # zvel
-
-    [0, 0, 0,   0, 0, 0,    apss.C[0][0], 0, 0,    apss.C[0][1], 0, 0], # xacc
-    [0, 0, 0,   0, 0, 0,    0, apss.C[0][0], 0,    0, apss.C[0][1], 0], # yacc
-    [0, 0, 0,   0, 0, 0,    0, 0, apss.C[0][0],    0, 0, apss.C[0][1]], # zacc
-
-    [0, 0, 0,   0, 0, 0,    apss.A[0][0], 0, 0,    apss.A[0][1], 0, 0], # xacc_cmd
-    [0, 0, 0,   0, 0, 0,    0, apss.A[0][0], 0,    0, apss.A[0][1], 0], # yacc_cmd
-    [0, 0, 0,   0, 0, 0,    0, 0, apss.A[0][0],    0, 0, apss.A[0][1]], # zacc_cmd
-
-    [0, 0, 0,   0, 0, 0,    apss.A[1][0], 0, 0,    apss.A[1][1], 0, 0], # xacc_cmd_dot
-    [0, 0, 0,   0, 0, 0,    0, apss.A[1][0], 0,    0, apss.A[1][1], 0], # yacc_cmd_dot
-    [0, 0, 0,   0, 0, 0,    0, 0, apss.A[1][0],    0, 0, apss.A[1][1]], # zacc_cmd_dot
-    ])
-
-# [ax, ay, az, ux, uy, uz]
-B = np.array([
-    [0, 0, 0,   0, 0, 0],
-    [0, 0, 0,   0, 0, 0],
-    [0, 0, 0,   0, 0, 0],
-    [1, 0, 0,   0, 0, 0],
-    [0, 1, 0,   0, 0, 0],
-    [0, 0, 1,   0, 0, 0],
-    [0, 0, 0,   *apss.B[0], 0, 0],
-    [0, 0, 0,   0, *apss.B[0], 0],
-    [0, 0, 0,   0, 0, *apss.B[0]],
-    [0, 0, 0,   *apss.B[1], 0, 0],
-    [0, 0, 0,   0, *apss.B[1], 0],
-    [0, 0, 0,   0, 0, *apss.B[1]],
-    ])
-
-C = np.eye(12)
-
-D = np.zeros((12, 6))
-
-ss = ct.ss(A, B, C, D)
 
 
 def atmosphere_model(t, X):
