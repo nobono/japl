6398b79f3dbfc3b0f76acd4d0455166582617d80
david <shincdavid@gmail.com>
2024-03-07 03:12:21 -0500

    popup skeleton working

--- main.py
+++ main.py
@@ -85,8 +85,17 @@ def guidance_uo_dive_func(t, rm, vm, r_targ):
     ASCEND_SPEED = 400.0
     START_ASCEND_RANGE = 45e3
     ASCEND_RATE_LIMIT = 100.0
-    START_DIVE_ALT = 5000.0
+    START_DIVE_ALT = 200.0
     STOP_DIVE_ALT = 20.0
+    #####################
+    ASCEND_SPEED_2 = 400.0
+    START_ASCEND_RANGE_2 = 32e3
+    ASCEND_RATE_LIMIT_2 = 100.0
+    START_DIVE_ALT_2 = 120.0
+    STOP_DIVE_ALT_2 = 20.0
+    #####################
+    START_TERMINAL_RANGE = 8e3
+    #####################
     K_SPEED = 0.05
     K_P = 0.05
     K_D = 0.06
@@ -101,6 +110,8 @@ def guidance_uo_dive_func(t, rm, vm, r_targ):
                 gd_phase += 1
         case 1 :
             # Ascend
+            K_P *= 2.0
+            K_D *= 3
             alt_dot = vm[2]
             ascend_rate = max(K_P * (START_DIVE_ALT - r_alt), -ASCEND_RATE_LIMIT)
             ac_alt = K_D * (ascend_rate - alt_dot)
@@ -110,7 +121,7 @@ def guidance_uo_dive_func(t, rm, vm, r_targ):
                 gd_phase += 1
         case 2 :
             # Descend
-            K_P *= 1.5
+            K_P *= 2.0
             K_D *= 5
             ac = np.zeros((3,))
             alt_dot = vm[2]
@@ -118,27 +129,45 @@ def guidance_uo_dive_func(t, rm, vm, r_targ):
             ac_alt = K_D * (ascend_rate - alt_dot)
             C_i_v = create_C_rot(vm)
             ac = C_i_v @ np.array([0, 0, ac_alt])
-            if r_alt >= STOP_DIVE_ALT:
+            if r_range <= START_ASCEND_RANGE_2:
                 gd_phase += 1
         case 3 :
-            # Level
-            K_D *= 7.0
-            ALTD = 20.0
-            ac = np.zeros((3,))
+            # Ascend
+            K_P *= 3.0
+            K_D *= 3
             alt_dot = vm[2]
-            ascend_rate = K_P * (ALTD - r_alt)
+            ascend_rate = max(K_P * (START_DIVE_ALT_2 - r_alt), -ASCEND_RATE_LIMIT_2)
             ac_alt = K_D * (ascend_rate - alt_dot)
             C_i_v = create_C_rot(vm)
             ac = C_i_v @ np.array([0, 0, ac_alt])
-            if r_range <= 8e3:
+            if r_alt >= START_DIVE_ALT_2:
                 gd_phase += 1
         case 4 :
-            # Ascend
+            # Descend
+            K_P *= 2.0
+            K_D *= 5
+            ac = np.zeros((3,))
             alt_dot = vm[2]
-            ascend_rate = max(K_P * (START_DIVE_ALT - r_alt), -ASCEND_RATE_LIMIT)
+            ascend_rate = min(K_P * (STOP_DIVE_ALT_2 - r_alt), ASCEND_RATE_LIMIT_2)
             ac_alt = K_D * (ascend_rate - alt_dot)
             C_i_v = create_C_rot(vm)
             ac = C_i_v @ np.array([0, 0, ac_alt])
+            if r_range <= START_TERMINAL_RANGE:
+                gd_phase += 1
+        # case 6 :
+        #     # Level
+        #     K_D *= 7.0
+        #     ALTD = 20.0
+        #     ac = np.zeros((3,))
+        #     alt_dot = vm[2]
+        #     ascend_rate = K_P * (ALTD - r_alt)
+        #     ac_alt = K_D * (ascend_rate - alt_dot)
+        #     C_i_v = create_C_rot(vm)
+        #     ac = C_i_v @ np.array([0, 0, ac_alt])
+        #     if r_range <= START_TERMINAL_RANGE:
+        #         gd_phase += 1
+        case 5 :
+            ac = guidance.pronav(rm, vm, r_targ, np.zeros((3,)), N=4)
         case _ :
             ac = np.zeros((3,))
             raise Exception("unhandled event")
@@ -180,7 +209,7 @@ t_span = [0, 300]
 dt = 0.01
 
 x0 = ss.get_init_state()
-x0[:3] = np.array([0, 50e3, 10])    #R0
+x0[:3] = np.array([0, 50e3, 20])    #R0
 x0[3:6] = np.array([0, -200, 0])   #V0
 
 targ_R0 = np.array([0, 0, 0])
