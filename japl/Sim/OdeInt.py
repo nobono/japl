f5ac1d1edc54870511c7063ea259b6f877767ce2
david.shin <david.shin@jhuapl.edu>
2024-06-20 19:22:53 -0400

    c extension odeint stuff

---
+++ japl/Sim/OdeInt.py
@@ -0,0 +1,16 @@
+# import numpy as np
+# from ctypes import (
+#     cdll,
+#     c_double,
+#     c_int,
+#     c_char_p,
+#     c_void_p,
+#     POINTER,
+#     byref,
+# )
+# dtype = np.float64
+
+
+
+# def to_carray(type, A: list):
+#     return (type * len(A))(*A)
--- setup.py
+++ setup.py
@@ -1,7 +1,14 @@
 from setuptools import setup
 from setuptools import find_packages
+from setuptools import Extension
 
 
+ode_int_ext = Extension(name="mylib",
+                        sources=["./japl/odeint.cpp"],
+                        # include_dirs=["C:/Users/shindc1/Documents/boost_1_82_0"],
+                        # extra_compile_args=[],
+                        # extra_link_args=["-shared"],
+                        )
 
 setup(
         name='japl',
@@ -9,6 +16,8 @@ setup(
         install_requires=[],
         packages=find_packages('.'),
         package_dir={'': '.'},
+        ext_modules=[ode_int_ext],
+        libraries=[],
         author="nobono",
         author_email="shincdavid@gmail.com",
         license="MIT",
