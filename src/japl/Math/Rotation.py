1fccd4cd85cc2f3bd10a9de2d7982d42745ce896
david <shincdavid@gmail.com>
2024-06-24 14:09:23 -0400

    refactor

--- src/japl/Math/Rotation.py
+++ src/japl/Math/Rotation.py
@@ -45,3 +45,48 @@ def quat_to_dcm(q: np.ndarray|quaternion) -> np.ndarray:
     return dcm
 
 
+def quat_to_tait_bryan(q: np.ndarray|quaternion):
+    if isinstance(q, quaternion):
+        q = q.components #type:ignore
+    yaw  = np.arctan2((q[2] * q[3] + q[0] * q[1]), 0.5 - (q[1]**2 + q[2]**2))
+    pitch = np.arcsin(-2 * (q[1] * q[3] - q[0] * q[2]))
+    roll  = np.arctan2((q[1] * q[2] + q[0] * q[3]), 0.5 - (q[2]**2 + q[3]**2))
+    # if abs(2 * (q[1] * q[3] + q[0] * q[2])) == 1.0:
+    #     print("gymbal lock conversion")
+    return np.array([yaw, pitch, roll])
+
+
+def tait_bryan_to_dcm(yaw_pitch_roll):
+    cyaw = np.cos(yaw_pitch_roll[0])
+    cpitch = np.cos(yaw_pitch_roll[1])
+    croll = np.cos(yaw_pitch_roll[2])
+    syaw = np.sin(yaw_pitch_roll[0])
+    spitch = np.sin(yaw_pitch_roll[1])
+    sroll = np.sin(yaw_pitch_roll[2])
+    dcm = np.array([
+        [cpitch*cyaw, sroll*spitch*cyaw - croll*syaw, sroll*syaw + croll*spitch*cyaw],
+        [cpitch*syaw, croll*cyaw + sroll*spitch*syaw, croll*spitch*syaw - sroll*cyaw],
+        [-spitch, sroll*cpitch, croll*cpitch],
+        ])
+    return dcm
+
+
+def dcm_to_tait_bryan(dcm):
+    # handle gimbal lock condition
+    if abs(abs(dcm[2][0]) - 1) < 1e-12:
+        # set roll to zero and solve for yaw
+        if dcm[2][0] < 0:
+            yaw = np.arctan2(-dcm[0][1], -dcm[0][2])
+            pitch = -np.arcsin(max(dcm[2][0], -1.0))
+            roll = 0
+        else:
+            yaw = np.arctan2(dcm[0][1], dcm[0][2])
+            pitch = -np.arcsin(min(dcm[2][0], 1.0))
+            roll = 0
+    else:
+        yaw = np.arctan2(dcm[1][0], dcm[0][0])
+        pitch = -np.arcsin(dcm[2][0])
+        roll = np.arctan2(dcm[2][1], dcm[2][2])
+    return np.array([yaw, pitch, roll])
+
+
