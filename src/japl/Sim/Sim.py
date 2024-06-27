d75a5ce430b2a4f551379943ddd3fe60d4f4d594
david <shincdavid@gmail.com>
2024-06-27 15:56:07 -0400

    moved device input get outside of step() func

--- src/japl/Sim/Sim.py
+++ src/japl/Sim/Sim.py
@@ -57,9 +57,13 @@ class Sim:
         # device inputs
         self.device_input_type = kwargs.get("device_input_type", "")
         self.device_input = DeviceInput(device_type=self.device_input_type)
+        self.device_input_data = {"lx": 0.0, "ly": 0.0}
 
         # atmosphere model
         self.atmosphere = Atmosphere()
+
+        # sim flags
+        self.flag_stop = False
 
         # debug stuff
         # TODO make this its own class so we can use
@@ -74,6 +78,9 @@ class Sim:
             if self.debug_profiler["count"] >= self.Nt:
                 print("ave_dt: %.5f, ave_Hz: %.1f" % (self.debug_profiler["t_ave"], (1 / self.debug_profiler["t_ave"])))
         self.debug_profiler = {"t": 0.0, "t_total": 0.0, "count": 0, "t_ave": 0.0, "run": _debug_profiler_func}
+
+        # TEMP internal plotting stuff
+        self.temp_data = {"pitch": [], "alpha": [], "iota": [], "q": [], "CN": [], "My": [], "Fz": []}
 
 
     def __instantiate_plot(self, **kwargs) -> None:
@@ -183,14 +190,13 @@ class Sim:
         torque_ext = np.array([0, 0, 0], dtype=float)
 
         mass = X[simobj.get_state_id("mass")]
-        iota = 0
+        iota = np.radians(10)
 
-        # get device input
+        # device input
         if self.device_input_type:
-            (lx, ly, _, _) = self.device_input.get()
-            iota = ly * 0.69
-            # force = np.array([1000*lx, 0, 1000*ly])
-            # acc_ext = acc_ext + force / mass
+            iota = -self.device_input_data["ly"] * 0.69
+        # force = np.array([1000*lx, 0, 1000*ly])
+        # acc_ext = acc_ext + force / mass
 
         ########################################################################
         # Aeromodel
@@ -215,18 +221,7 @@ class Sim:
             flight_path_angle = vec_ang(vel_hat, np.array([1, 0, 0]))
             alpha = pitch_angle - flight_path_angle                     # angle of attack
 
-            # lookup coefficients
-            if alpha > 0:
-                CLMB = simobj.aerotable.get_CLMB_Total(alpha, phi, mach, iota)
-                CNB = simobj.aerotable.get_CNB_Total(alpha, phi, mach, iota)
-            else:
-                CLMB = -simobj.aerotable.get_CLMB_Total(-alpha, phi, mach, iota)
-                CNB = -simobj.aerotable.get_CNB_Total(-alpha, phi, mach, iota)
-
-            y_moment_coef = CLMB + (simobj.cg - simobj.aerotable.MRC[0]) * CNB
-
-            # CNB = simobj.aerotable.get_CNB_Total(alpha, phi, mach, iota)
-
+            ###################################################################
             # pitching moment coeficient for iota increments
             # My_coef = model.CLM(alpha,mach)+...
             #           model.cmit(alpha*n,mach*n,model.increments.iota)+...
@@ -234,29 +229,62 @@ class Sim:
             #          (model.CN(alpha,mach)+...
             #           model.CNit(alpha*n,mach*n,model.increments.iota));
 
-            # calulate moments: (M_coef * q * Sref * Lref), where:
-            #       M_coef - moment coeficient
-            #       q      - dynamic pressure
-            #       Sref   - surface area reference (wing area)
-            #       Lref   - length reference (mean aerodynamic chord)
-            q = self.atmosphere.dynamic_pressure(vel, alt)
-            ytorque = y_moment_coef * q * simobj.aerotable.Sref * simobj.aerotable.Lref
-            # zforce = CNB /
+            # % get dynamic pressure (N/m^2)
+            # qbar = (model.conv.lbf2n/model.conv.ft2m^2)*...
+            #         computeqbar(mach,model.conv.m2ft*alt);
 
-            # update external moments
-            torque_ext[1] = ytorque
+            # Fvec = (qbar*model.sref)*...
+            #        ([-model.CA_Basic(alpha,mach)+...
+            #          -model.CA0(mach,alt,boost);...
+            #          -model.CN(alpha,mach)]+...% basic stability
+            #         [-model.cadit(alpha, mach, iota);...
+            #          -model.CNit(alpha, mach, iota)]);% b-plane steering
+            #
+            # My   = (qbar*model.sref*model.lref)*...
+            #        (model.CLM(alpha, mach)+...       % basic stability
+            #         model.cmit(alpha, mach, iota));   % b-plane steering
+            ###################################################################
+
+            # alpha = np.radians(-30)
+
+            # lookup coefficients
+            try:
+                CLMB = simobj.aerotable.get_CLMB_Total(alpha, phi, mach, iota)
+                CNB = simobj.aerotable.get_CNB_Total(alpha, phi, mach, iota)
+
+                My_coef = CLMB + (simobj.cg - simobj.aerotable.MRC[0]) * CNB
+
+                # calulate moments: (M_coef * q * Sref * Lref), where:
+                #       M_coef - moment coeficient
+                #       q      - dynamic pressure
+                #       Sref   - surface area reference (wing area)
+                #       Lref   - length reference (mean aerodynamic chord)
+                q = self.atmosphere.dynamic_pressure(vel, alt)
+                My = My_coef * q * simobj.aerotable.Sref * simobj.aerotable.Lref
+                zforce = CNB * q * simobj.aerotable.Sref
+
+                # update external moments
+                # (positive )
+                torque_ext[1] = My / simobj.Iyy
+                acc_ext[2] = acc_ext[2] + zforce / mass
+                # print(torque_ext[1], acc_ext[2], q)
+
+                # {"pitch": [], "aoa": [], "iota": [], "q": [], "CN": [], "My": [], "Fz": []}
+                self.temp_data["q"] += [q]
+                self.temp_data["pitch"] += [pitch_angle]
+                self.temp_data["alpha"] += [alpha]
+                self.temp_data["iota"] += [iota]
+                self.temp_data["CN"] += [CNB]
+                self.temp_data["My"] += [My]
+                self.temp_data["Fz"] += [zforce]
+
+            except Exception as e:
+                self.flag_stop = True
+
         ########################################################################
-
-        # fuel_burn = X[6]
-        # if fuel_burn < 100:
-        #     ac[0] += 20
-        #     ac[2] += 40
-
-        burn_const = 0.4
 
         U = np.concatenate([acc_ext, torque_ext])
         Xdot = simobj.step(X, U)
-        # Xdot[6] = burn_const * np.linalg.norm(ac) #type:ignore
 
         return Xdot
 
@@ -293,6 +321,12 @@ class Sim:
         self.debug_profiler["run"]()
         #########################
 
+        # get device input
+        if self.device_input_type:
+            (lx, ly, _, _) = self.device_input.get()
+            self.device_input_data["lx"] = lx
+            self.device_input_data["ly"] = ly
+
         tstep_prev = self.t_array[istep - 1]
         tstep = self.t_array[istep]
         x0 = _simobj.Y[istep - 1]
@@ -311,4 +345,8 @@ class Sim:
         self.T[istep] = sol['t'][0]
         _simobj.Y[istep] = sol['y'].T[0]
 
+        # TODO do this better...
+        if self.flag_stop:
+            if self.animate:
+                self.plotter.istep = self.Nt
 
