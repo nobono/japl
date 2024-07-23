d93e775244b5b0361ad0221e2c50e91890413b42
david <shincdavid@gmail.com>
2024-07-23 16:31:53 -0400

    added example main test

--- test_main.py
+++ test_main.py
@@ -1,99 +1,171 @@
-import matplotlib.pyplot as plt
 import unittest
-import control as ct
 import numpy as np
+import quaternion
 from japl import Model
 from japl import SimObject
+from japl import Sim
+from japl import AeroTable
 
 
 
 class TestExample(unittest.TestCase):
 
-    def test_example(self):
+    def __check(self, vehicle: SimObject):
+        # for i in vehicle.Y[-1]:
+        #     print(i)
+        truth = np.array([
+            1500.0000000000002,
+            0.0,
+            10000.003387596103,
+            1500.0,
+            0.0,
+            0.01360428568910825,
+            0.0,
+            0.008934050694513255,
+            0.0,
+            0.9999992228258203,
+            0.0,
+            0.0012467348195054982,
+            0.0,
+            133.0,
+            ])
+        self.assertTrue((vehicle.Y[-1] == truth).all())
 
-        Tend = .1
-        dt = 0.001
-        N = int(Tend / dt)
+
+    def __build_model(self) -> Model:
+        model = Model()
+
+        x  = model.add_state("x",         0,  "x (m)")
+        y  = model.add_state("y",         1,  "y (m)")
+        z  = model.add_state("z",         2,  "z (m)")
+        vx = model.add_state("vx",        3,  "xvel (m/s)")
+        vy = model.add_state("vy",        4,  "yvel (m/s)")
+        vz = model.add_state("vz",        5,  "zvel (m/s)")
+        wx = model.add_state("wx",        6,  "wx (rad/s)")
+        wy = model.add_state("wy",        7,  "wy (rad/s)")
+        wz = model.add_state("wz",        8,  "wz (rad/s)")
+        q0 = model.add_state("q0",        9,  "q0")
+        q1 = model.add_state("q1",        10, "q1")
+        q2 = model.add_state("q2",        11, "q2")
+        q3 = model.add_state("q3",        12, "q3")
+
+        mass = model.add_state("mass",    13, "mass (kg)")
+
+        Sq = np.array([
+            [-q1, -q2, -q3],
+            [q0, -q3, q2],
+            [q3, q0, -q1],
+            [-q2, q1, q0],
+            ]) * 0.5
 
         A = np.array([
-            [0, 1],
-            [0, 0],
+            [0,0,0,  1,0,0,  0,0,0,  0,0,0,0,  0], # x
+            [0,0,0,  0,1,0,  0,0,0,  0,0,0,0,  0], # y
+            [0,0,0,  0,0,1,  0,0,0,  0,0,0,0,  0], # z
+            [0,0,0,  0,0,0,  0,0,0,  0,0,0,0,  0], # vx
+            [0,0,0,  0,0,0,  0,0,0,  0,0,0,0,  0], # vy
+            [0,0,0,  0,0,0,  0,0,0,  0,0,0,0,  0], # vz
+
+            [0,0,0,  0,0,0,  0,0,0,  0,0,0,0,  0], # wx
+            [0,0,0,  0,0,0,  0,0,0,  0,0,0,0,  0], # wy
+            [0,0,0,  0,0,0,  0,0,0,  0,0,0,0,  0], # wz
+
+            [0,0,0,  0,0,0,  *Sq[0], 0,0,0,0,  0], # q0
+            [0,0,0,  0,0,0,  *Sq[1], 0,0,0,0,  0], # q1
+            [0,0,0,  0,0,0,  *Sq[2], 0,0,0,0,  0], # q2
+            [0,0,0,  0,0,0,  *Sq[3], 0,0,0,0,  0], # q3
+
+            [0,0,0,  0,0,0,  0,0,0,  0,0,0,0,  0], # mass
             ])
+
         B = np.array([
-            [0],
-            [1],
+            # force  torque
+            [0,0,0,  0,0,0],
+            [0,0,0,  0,0,0],
+            [0,0,0,  0,0,0],
+            [1,0,0,  0,0,0],
+            [0,1,0,  0,0,0],
+            [0,0,1,  0,0,0],
+
+            [0,0,0,  1,0,0],
+            [0,0,0,  0,1,0],
+            [0,0,0,  0,0,1],
+
+            [0,0,0,  0,0,0],
+            [0,0,0,  0,0,0],
+            [0,0,0,  0,0,0],
+            [0,0,0,  0,0,0],
+
+            [0,0,0,  0,0,0],
             ])
 
-        model = Model.ss(A, B)
+        model.ss(A, B)
 
-        omega = 200
-        zeta = .3
-        act = ct.tf2ss([1], [1/omega**2, 2*zeta/omega, 1])
+        return model
+        
 
+    def test_example_case1(self):
 
-        x1 = np.array([0, 0])
-        x2 = np.array([0, 0])
+        model = self.__build_model()
+        vehicle = SimObject(model=model, size=2, color='tab:blue')
+        vehicle.aerotable = AeroTable("./aeromodel/aeromodel_psb.mat")
 
-        U = [1] * N
+        vehicle.plot.set_config({
+                    "Pos": {
+                        "xaxis": "x",
+                        "yaxis": "z",
+                        "aspect": "auto",
+                        },
+                    "Vel": {
+                        "xaxis": "t",
+                        "yaxis": "vz",
+                        "aspect": "auto",
+                        },
+                    })
 
-        T = []
-        Y = []
-        Y_vehicle = []
-        t = 0
+        # Inits
+        ####################################
 
-        for i in range(N):
+        vehicle.Ixx = 1.309 # (kg * m^2)
+        vehicle.Iyy = 58.27 # (kg * m^2)
+        vehicle.Izz = 58.27 # (kg * m^2)
+        vehicle.mass = 133 # (kg)
+        vehicle.cg = 1.42 # (m)
+        x0 = [0, 0, 10000]
+        v0 = [1500, 0, 0]
+        w0 = [0, 0, 0]
+        quat0 = quaternion.from_euler_angles([0, 0, 0]).components
+        mass0 = 133.0
+        vehicle.init_state([x0, v0, w0, quat0, mass0]) # TODO this should be moved to Model
 
-            t += dt
-            T += [t]
+        # Sim
+        ####################################
 
-            # update actuator model
-            Y += [act.C @ x1]
-            u1 = np.array([U[i]])
-            x1_dot = act.A @ x1 + act.B @ u1
-            x1 = x1_dot * dt + x1
+        # TODO dt is refresh rate for animation
+        # but dt just create t_array for no animation
+        sim = Sim(
+                t_span=[0, 1],
+                dt=.01,
+                simobjs=[vehicle],
+                integrate_method="rk4",
+                events=[],
+                animate=1,
+                aspect="equal",
+                device_input_type="",
+                moving_bounds=True,
+                rtol=1e-6,
+                atol=1e-6,
+                blit=False,
+                antialias=0,
+                figsize=(10, 7),
+                instrument_view=1,
+                draw_cache_mode=0,
+                quiet=1, # TODO still working on this
+                )
 
-            # update vehicle model
-            x2_dot = model.step(x2, (act.C @ x1))
-            x2 = x2_dot * dt + x2
-            Y_vehicle += [x2]
+        sim.run()
 
-
-        Y = np.asarray(Y)
-        Y_vehicle = np.asarray(Y_vehicle)
-
-        print(Y_vehicle[-1, 0])
-
-        # ------------------------
-
-        x1 = np.array([0, 0])
-        x2 = np.array([0, 0])
-
-        A1 = act.A
-        B1 = act.B
-        C1 = act.C
-
-        A2 = np.array([
-            [0, 1],
-            [0, 0],
-            ])
-        B2 = np.array([
-            [0],
-            [1],
-            ])
-
-        A = np.block([
-            [A1, np.zeros((2, 2))],
-            [(B2 @ C1), A2],
-            ])
-
-        # plt.plot(T, [1] * N)
-        # plt.plot(T, Y[:, 0])
-        # plt.figure()
-        # plt.plot(T, Y_vehicle[:, 0])
-        # plt.figure()
-        # plt.plot(T, Y_vehicle[:, 1])
-        # plt.show()
-
+        self.__check(vehicle)
                 
 
 
