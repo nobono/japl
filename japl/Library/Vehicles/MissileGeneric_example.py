f506a85908c32ca2c5a4defa5d26076baf23efc9
david <shincdavid@gmail.com>
2024-08-02 14:09:50 -0400

    cleanup MissileGeneric

--- japl/Library/Vehicles/MissileGeneric_example.py
+++ japl/Library/Vehicles/MissileGeneric_example.py
@@ -1,12 +1,52 @@
 from pprint import pprint
 import numpy as np
-from sympy import Expr, Matrix, MatrixSymbol, symbols, Piecewise
+from sympy import Expr, Matrix, MatrixSymbol, Symbol, symbols, Piecewise
 from sympy import sqrt
+from sympy.matrices.expressions.matexpr import MatrixElement
 from japl import Model
 from japl.Math.MathSymbolic import zero_protect_sym
 from sympy import Function
 from sympy import simplify
+from japl import StateRegister
 
+
+
+def process_subs(sub: tuple|list|dict) -> dict:
+    """This method is used to convert differntial definitions
+    into a substitutable dict."""
+    ret = {}
+    if isinstance(sub, tuple) or isinstance(sub, list):
+        # update a new dict with substition pairs.
+        # if pair is Matrix or MatrixSymbol (N x 1),
+        # update each element.
+        for old, new in sub:
+            if hasattr(old, "__len__") and hasattr(new, "__len__"):
+                for elem_old, elem_new in zip(old, new): #type:ignore
+                    ret[elem_old] = elem_new
+            else:
+                try:
+                    ret[old] = new
+                except Exception as e:
+                    raise Exception(e, "\nunhandled case. old and new need to both have '__len__'.")
+    else:
+        ret = sub
+    return ret
+
+
+def process_var_definition(sub: tuple|list|Matrix) -> dict:
+    """This method generates a 'subs' dict from provided
+    symbolic variables (Symbol, Function, Matrix). This is
+    used for substition of variables into a sympy expression."""
+    assert hasattr(sub, "__len__")
+    ret = {}
+    # for each element get the name
+    for var in sub:
+        if hasattr(var, "__len__"): # if Matrix
+            for elem in var: #type:ignore
+                ret[elem] = StateRegister._process_variables(elem)
+        else:
+            ret[var] = StateRegister._process_variables(var)
+    return ret
 
 
 class MissileGeneric(Model):
@@ -26,7 +66,7 @@ q_0, q_1, q_2, q_3 = symbols("q_0, q_1, q_2, q_3", cls=Function) #type:ignore
 
 pos = Matrix([pos_x(t), pos_y(t), pos_z(t)])
 vel = Matrix([vel_x(t), vel_y(t), vel_z(t)])
-w = Matrix([w_x(t), w_y(t), w_z(t)])
+angvel = Matrix([w_x(t), w_y(t), w_z(t)])
 q = Matrix([q_0(t), q_1(t), q_2(t), q_3(t)])
 mass = symbols("mass")
 gravity = Matrix(symbols("gravity_x gravity_y gravity_z"))
@@ -38,7 +78,7 @@ speed = symbols("speed")
 acc = Matrix(symbols("acc_x acc_y acc_z"))
 torque = Matrix(symbols("torque_x torque_y torque_z"))
 
-wx, wy, wz = w
+wx, wy, wz = angvel
 Sw = Matrix([
     [ 0,   wx,  wy,  wz], #type:ignore
     [-wx,  0,  -wz,  wy], #type:ignore
@@ -48,49 +88,18 @@ Sw = Matrix([
 
 pos_new = pos + vel * dt
 vel_new = vel + (acc + gravity) * dt
-w_new = w + torque * dt
+angvel_new = angvel + torque * dt
 q_new = q + (-0.5 * Sw * q) * dt
 
 ##################################################
 # Subs for differential definitions
 ##################################################
-diffsub = (
-           (pos[0].diff(t), pos_new[0].diff(dt)),    #type:ignore
-           (pos[1].diff(t), pos_new[1].diff(dt)),    #type:ignore
-           (pos[2].diff(t), pos_new[2].diff(dt)),    #type:ignore
-           (vel[0].diff(t), vel_new[0].diff(dt)),    #type:ignore
-           (vel[1].diff(t), vel_new[1].diff(dt)),    #type:ignore
-           (vel[2].diff(t), vel_new[2].diff(dt)),    #type:ignore
-           (w[0].diff(t), w_new[0].diff(dt)),        #type:ignore
-           (w[1].diff(t), w_new[1].diff(dt)),        #type:ignore
-           (w[2].diff(t), w_new[2].diff(dt)),        #type:ignore
-           (q[0].diff(t), q_new[0].diff(dt)),        #type:ignore
-           (q[1].diff(t), q_new[1].diff(dt)),        #type:ignore
-           (q[2].diff(t), q_new[2].diff(dt)),        #type:ignore
-           (q[3].diff(t), q_new[3].diff(dt)),        #type:ignore
-           )
-
-##################################################
-# Subs for final state vars
-##################################################
-varsub = {
-        pos[0]: symbols("pos_x"),           #type:ignore
-        pos[1]: symbols("pos_y"),           #type:ignore
-        pos[2]: symbols("pos_z"),           #type:ignore
-        vel[0]: symbols("vel_x"),           #type:ignore
-        vel[1]: symbols("vel_y"),           #type:ignore
-        vel[2]: symbols("vel_z"),           #type:ignore
-        w[0]: symbols("angvel_x"),          #type:ignore
-        w[1]: symbols("angvel_y"),          #type:ignore
-        w[2]: symbols("angvel_z"),          #type:ignore
-        q[0]: symbols("q_0"),               #type:ignore
-        q[1]: symbols("q_1"),               #type:ignore
-        q[2]: symbols("q_2"),               #type:ignore
-        q[3]: symbols("q_3"),               #type:ignore
-        torque[0]: symbols("torque_x"),     #type:ignore
-        torque[1]: symbols("torque_y"),     #type:ignore
-        torque[2]: symbols("torque_z"),     #type:ignore
-        }
+diff_definition = (
+        (pos.diff(t), pos_new.diff(dt)),
+        (vel.diff(t), vel_new.diff(dt)),
+        (angvel.diff(t), angvel_new.diff(dt)),
+        (q.diff(t), q_new.diff(dt)),
+        )
 
 speed_new = (vel.dot(vel))**0.5
 
@@ -100,44 +109,31 @@ speed_new = (vel.dot(vel))**0.5
 X_new = Matrix([
     pos,
     vel,
-    w_new.as_mutable(),
-    q_new.as_mutable(),
+    angvel,
+    q,
     mass,
     gravity,
     speed_new,
     ])
 
-state = Matrix([pos, vel, w, q, mass, gravity, speed])
+state = Matrix([pos, vel, angvel, q, mass, gravity, speed])
 input = Matrix([acc, torque])
 
 
 ##################################################
-# Issues observed passing subs as tuple of tuples.
-# ensure the subs argument is formated as dict.
+# Process differential & state definitions
+# to substition format
 ##################################################
-# diffsub = ((pos.diff(t), pos_new.diff(dt)), )
-diffsub_dict = {}
-if isinstance(diffsub, tuple) or isinstance(diffsub, list):
-    # update a new dict with substition pairs.
-    # if pair is Matrix or MatrixSymbol (N x 1),
-    # update each element.
-    for old, new in diffsub:
-        if hasattr(old, "__len__") and hasattr(new, "__len__"):
-            for elem_old, elem_new in zip(old, new): #type:ignore
-                diffsub_dict[elem_old] = elem_new
-        else:
-            try:
-                diffsub_dict[old] = new
-            except Exception as e:
-                raise Exception(e, "\nunhandled case. old and new need to both have '__len__'.")
-else:
-    diffsub_dict = diffsub
+diff_sub = process_subs(diff_definition)
+state_sub = process_var_definition(state)
+input_sub = process_var_definition(input)
 
 ##################################################
 # Define dynamics
 ##################################################
-dynamics = Matrix(X_new.diff(t).subs(diffsub_dict).doit())
-dynamics = dynamics.subs(varsub)
+dynamics = Matrix(X_new.diff(t))
+dynamics = dynamics.subs(diff_sub).doit()
+dynamics = dynamics.subs(state_sub).subs(input_sub)
 
 
 model = MissileGeneric().from_expression(dt, state, input, dynamics)
