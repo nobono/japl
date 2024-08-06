4b1c7a2733a2b4fa360e14985b95f60bf012f07c
david <shincdavid@gmail.com>
2024-08-06 02:08:16 -0400

    refactor DirectUpdate

--- japl/BuildTools/DirectUpdate.py
+++ japl/BuildTools/DirectUpdate.py
@@ -1,33 +1,20 @@
 # from sympy.core.function import FunctionClass, UndefinedFunction
-from typing import Optional
+from typing import Union
 from sympy import Symbol
-from sympy import Expr, Matrix, symbols, Function
+from sympy import Expr, Matrix, Function
 from sympy.matrices.expressions.matexpr import MatrixElement
 from sympy import zeros as sympy_zeros
 
+DUType = Union[Symbol, Matrix, MatrixElement, Function, list]
 
-# class DirectUpdate(tuple):
-
-#     """This class allows for direct settings of the Model state."""
-
-    # def __new__(cls, *args):
-    #     args = DirectUpdate.process_args(*args)
-    #     return super(DirectUpdate, cls).__new__(cls, args)
-
-        
-# class DirectUpdate(Expr):
-#     def __new__(cls, var: Symbol|Matrix|Function, val, **assumptions):
-#         obj = super().__new__(cls, var, val, **assumptions) #type:ignore
-#         obj.subs = DirectUpdate.create_subs(var, val) #type:ignore
-#         return obj
 
 
 class DirectUpdateSymbol(Symbol):
 
 
-    def __init__(self, name, **assumptions):
-        self.state_expr: Optional[Expr] = None
-        self.sub_expr: Optional[Expr] = None
+    def __init__(self, name: str, state_expr: Expr, sub_expr: Expr, **assumptions):
+        self.state_expr = state_expr
+        self.sub_expr = sub_expr
         return super().__init__()
 
 
@@ -39,89 +26,34 @@ class DirectUpdate(Matrix):
             which contains the "expr" attribute used to update the state during the
             Sim.step()."""
 
-    def __new__(cls, var: Symbol|Matrix|MatrixElement|Function|list, val, **assumptions):
-        var = cls.get_symbol(var)
-        if var.__class__ in [Symbol, Function]:
-            obj = super().__new__(cls, Matrix([var]), **assumptions) #type:ignore
-        else:
-            obj = super().__new__(cls, var, **assumptions) #type:ignore
-        obj.update_map = cls.create_update_map(var, val) #type:ignore
+    def __new__(cls, var: DUType, val: DUType, **assumptions):
+        var = cls.process_symbols(var, val)
+        obj = super().__new__(cls, var, **assumptions) #type:ignore
         return obj
 
 
     def diff(self, *args, **kwargs) -> Matrix:
-        """Direct update of the state will force any derivative
+        """Overload diff() of DirectUpdate to force any derivative
         to zero."""
         return sympy_zeros(*self.shape)
 
 
     @staticmethod
-    def get_symbol(var):
+    def process_symbols(var, val) -> Matrix:
         """Takes Matrix, Function, Symbol and returns DirectUpdateSymbol."""
         if isinstance(var, Symbol):
             name = f"DU({str(var)})"
-            ret = DirectUpdateSymbol(name)
-            ret.state_expr = var
-            return ret
+            return Matrix([DirectUpdateSymbol(name, state_expr=var, sub_expr=val)])
         match var.__class__():
             case Function():
                 name = f"DU({str(var)})"
-                ret = DirectUpdateSymbol(name) #type:ignore
-                ret.state_expr = var
-                return ret
+                return Matrix([DirectUpdateSymbol(name, state_expr=var, sub_expr=val)])
             case Matrix():
-                return [DirectUpdate.get_symbol(elem) for elem in var]
+                return Matrix([DirectUpdate.process_symbols(var_elem, val_elem)\
+                        for var_elem, val_elem in zip(var, val)])
             case list():
-                return [DirectUpdate.get_symbol(elem) for elem in var]
+                return Matrix([DirectUpdate.process_symbols(var_elem, val_elem)\
+                        for var_elem, val_elem in zip(var, val)])
             case _:
                 raise Exception("unhandled case.")
 
-
-    @staticmethod
-    def create_update_map(var, val) -> tuple:
-        """sets DirectUpdateSymbol.expr and return update_map"""
-        if isinstance(var, DirectUpdateSymbol):
-            var.sub_expr = val
-            return (var, val)
-        match var.__class__():
-            case Expr():
-                var.sub_expr = val
-                return (var, val)
-            case Function():
-                var.sub_expr = val
-                return (var, val)
-            case Matrix():
-                assert hasattr(val, "__len__")
-                for i, j in zip(var, val):
-                    i.sub_expr = j
-                return [(i, j) for i, j in zip(var, val)] #type:ignore
-            case list():
-                assert hasattr(val, "__len__")
-                for i, j in zip(var, val):
-                    i.sub_expr = j
-                return [(i, j) for i, j in zip(var, val)] #type:ignore
-            case _:
-                raise Exception("unhandled case.")
-
-
-# t = symbols('t')
-# x, y, z = symbols('x y z', cls=Function) #type:ignore
-# vx, vy, vz = symbols('vx vy vz', cls=Function) #type:ignore
-# pos = Matrix([x(t), y(t), z(t)])
-# vel = Matrix([vx(t), vy(t), vz(t)])
-# # ret = DirectUpdate(pos[0], vel[0])
-# # ret = DirectUpdate(x(t), y(t))
-# ret = DirectUpdate(pos, vel)
-# print(ret)
-# print(ret.subs)
-# # print(ret.diff(t))
-# # print(ret.expr)
-# # print(ret, ret.__class__)
-# # print(ret.subs)
-
-# x = DirectUpdateSymbol('x')
-# y = DirectUpdateSymbol('y')
-# z = DirectUpdateSymbol('z')
-# p,q,r = symbols("p q r")
-# mat = DirectUpdate([x, y, z], [p, q, r])
-# pass
