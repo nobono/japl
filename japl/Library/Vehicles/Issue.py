3bf06cde822ae54c322630bd5a2a1c9e92480a2f
david <shincdavid@gmail.com>
2024-08-15 12:07:28 -0400

    still figuring stuff out

--- japl/Library/Vehicles/Issue.py
+++ japl/Library/Vehicles/Issue.py
@@ -127,53 +127,176 @@ gravity = Matrix([0, 0, gacc_new])
 ##################################################
 # AeroTable
 ##################################################
+def aerotable_update_func(pos, vel, quat, cg, Iyy, speed, mach):
+    sos = atmosphere.speed_of_sound(pos[2]) #type:ignore
+    mach_new = speed / sos #type:ignore
 
-sos = atmosphere.speed_of_sound(pos[2]) #type:ignore
-mach_new = speed / sos #type:ignore
+    # calc angle of attack: (pitch_angle - flight_path_angle)
+    vel_hat = vel / speed   # flight path vector
 
-# calc angle of attack: (pitch_angle - flight_path_angle)
-vel_hat = vel / speed   # flight path vector
+    # projection vel_hat --> x-axis
+    zx_plane_norm = Matrix([0, 1, 0])
+    vel_hat_zx = ((vel_hat.dot(zx_plane_norm)) / zx_plane_norm.norm()) * zx_plane_norm
+    vel_hat_proj = vel_hat - vel_hat_zx
 
-# projection vel_hat --> x-axis
-zx_plane_norm = Matrix([0, 1, 0])
-vel_hat_zx = ((vel_hat.dot(zx_plane_norm)) / zx_plane_norm.norm()) * zx_plane_norm
-vel_hat_proj = vel_hat - vel_hat_zx
+    # get Trait-bryan angles (yaw, pitch, roll)
+    yaw_angle, pitch_angle, roll_angle = RotationSymbolic.quat_to_tait_bryan_sym(quat)
 
-# get Trait-bryan angles (yaw, pitch, roll)
-yaw_angle, pitch_angle, roll_angle = RotationSymbolic.quat_to_tait_bryan_sym(quat)
+    # angle between proj vel_hat & xaxis
+    x_axis_inertial = Matrix([1, 0, 0])
+    flight_path_angle = sign(vel_hat_proj[2]) * VecSymbolic.vec_ang_sym(vel_hat_proj, x_axis_inertial) #type:ignore
+    alpha_new = pitch_angle - flight_path_angle # angle of attack
+    phi_new = roll_angle
 
-# angle between proj vel_hat & xaxis
-x_axis_inertial = Matrix([1, 0, 0])
-flight_path_angle = sign(vel_hat_proj[2]) * print_sym(VecSymbolic.vec_ang_sym(vel_hat_proj, x_axis_inertial)) #type:ignore
-alpha_new = pitch_angle - flight_path_angle # angle of attack
-phi_new = roll_angle
+    iota = sp.Float(0)
+    CLMB = -aerotable.get_CLMB_Total(alpha_new, phi_new, mach, iota) #type:ignore
+    CNB = aerotable.get_CNB_Total(alpha_new, phi_new, mach, iota) #type:ignore
+    My_coef = CLMB + (cg - aerotable.get_MRC()) * CNB #type:ignore
+
+    q = atmosphere.dynamic_pressure(vel, pos_z) #type:ignore
+    Sref = aerotable.get_Sref()
+    Lref = aerotable.get_Lref()
+    My = My_coef * q * Sref * Lref
+
+    force_z_aero = CNB * q * Sref #type:ignore
+    force_aero = Matrix([0, 0, force_z_aero])
+
+    torque_y_aero = My / Iyy
+    torque_aero = Matrix([0, torque_y_aero, 0])
+
+    # temp
+    # force_aero = Matrix([0, 0, 1])
+    # torque_aero = Matrix([0, 1, 0])
+
+    extra = torque_y_aero
+
+    return (alpha_new, phi_new, force_aero, torque_aero, extra)
+
+######################
+import numpy as np
+from japl.Math import Rotation
+from japl.Math import Vec
+from japl.Aero.AeroTable import AeroTable
+from japl.Aero.Atmosphere import Atmosphere
+aero = AeroTable(aero_file)
+atmos = Atmosphere()
+
+def aerotable_update(pos, vel, quat, cg, Iyy, speed, mach):
+
+    alt = pos[2]
+
+    # calc angle of attack: (pitch_angle - flight_path_angle)
+    vel_hat = vel / speed
+
+    # projection vel_hat --> x-axis
+    zx_plane_norm = np.array([0, 1, 0])
+    vel_hat_zx = ((vel_hat @ zx_plane_norm) / np.linalg.norm(zx_plane_norm)) * zx_plane_norm
+    vel_hat_proj = vel_hat - vel_hat_zx
+
+    # get Trait-bryan angles (yaw, pitch, roll)
+    yaw_angle, pitch_angle, roll_angle = Rotation.quat_to_tait_bryan(np.asarray(quat))
+
+    # angle between proj vel_hat & xaxis
+    x_axis_inertial = np.array([1, 0, 0])
+    flight_path_angle = np.sign(vel_hat_proj[2]) * Vec.vec_ang(vel_hat_proj, x_axis_inertial)
+    alpha_new = pitch_angle - flight_path_angle                     # angle of attack
+    phi_new = roll_angle
+
+    iota = 0.0
+    CLMB = -aero.get_CLMB_Total(alpha_new, phi_new, mach, iota) #type:ignore
+    CNB = aero.get_CNB_Total(alpha_new, phi_new, mach, iota) #type:ignore
+    My_coef = CLMB + (cg - aero.get_MRC()) * CNB #type:ignore
+
+    q = atmos.dynamic_pressure(vel, alt) #type:ignore
+    Sref = aero.get_Sref()
+    Lref = aero.get_Lref()
+    My = My_coef * q * Sref * Lref
+
+    force_z = CNB * q * Sref #type:ignore
+    force_aero = np.array([0, 0, force_z])
+
+    torque_y_new = My / Iyy
+    torque_aero = np.array([0, torque_y_new, 0])
+
+    # temp
+    # force_aero = np.array([0, 0, 1])
+    # torque_aero = np.array([0, 1, 0])
+
+    extra = torque_y_new
+
+    return (alpha_new, phi_new, force_aero, torque_aero,
+            extra,
+            )
+
+# pos_ = np.array([0, 0, 0])
+# vel_ = np.array([1500, 0, 0])
+# quat_ = np.array([1, 0, 0, 0])
+# cg_ = 1.4
+# Iyy_ = 58.0
+# speed_ = 1500
+# mach_ = speed_ / 343.0
+
+pos_ = np.array([ 3.00000000e+01,  0.00000000e+00,  9.99999902e+03])
+vel_ = np.array([1.50000000e+03, 0.00000000e+00, -1.95366993e-01, ])
+# angvel_ = [ 0.00000000e+00,  3.43229792e-04, 0.00000000e+00,]
+quat_ = np.array([ 1.00000000e+00, -0.00000000e+00,  8.58074481e-07, 0.00000000e+00, ])
+# mass_ = 1.33000000e+02
+cg_ = 1.42000000e+00
+# Ixx_ = 1.30900000e+00
+Iyy_ = 5.82700000e+01
+# Izz_ = 5.82700000e+01
+# gacc_ = -9.81000000e+00
+speed_ = 1.50000001e+03
+mach_ = 5.00781791e+00
+alpha_ = 1.31960810e-04
+phi_ = 0.00000000e+00
+# extra _ = 0.00000000e+00
+iota_ = 0.0
 
 iota = sp.Float(0)
-CLMB = -aerotable.get_CLMB_Total(alpha_new, phi_new, mach, iota) #type:ignore
-CNB = aerotable.get_CNB_Total(alpha_new, phi_new, mach, iota) #type:ignore
-My_coef = CLMB + (cg - aerotable.get_MRC()) * CNB #type:ignore
 
-q = atmosphere.dynamic_pressure(vel, pos_z) #type:ignore
-Sref = aerotable.get_Sref()
-Lref = aerotable.get_Lref()
-My = My_coef * q * Sref * Lref
+(
+alpha_new_,
+phi_new_,
+force_aero_,
+torque_aero_,
+extra_
+) = aerotable_update(pos_, vel_, quat_, cg_, Iyy_, speed_, mach_)
+# print(alpha_new_, phi_new_, force_aero_, torque_aero_, extra_)
 
-force_z_aero = CNB * q * Sref #type:ignore
-force_aero = Matrix([0, 0, force_z_aero])
+(
+alpha_new2_,
+phi_new2_,
+force_aero2_,
+torque_aero2_,
+extra2_
+) = aerotable_update_func(pos, vel, quat, cg, Iyy, speed, mach)
 
-torque_y_aero = My / Iyy
-torque_aero = Matrix([0, torque_y_aero, 0])
 
-# temp
-force_aero = Matrix([0, 0, 1])
-torque_aero = Matrix([0, 1, 0])
+from japl.Util.Desym import Desym
+vars = (pos, vel, quat, cg, Iyy, speed, mach)
+vars_ = (pos_, vel_, quat_, cg_, Iyy_, speed_, mach_)
 
-# TODO: diff is with CNB. CNB args diff is alpha_new
-# when looking at alpha_new here:
-#       CNB = aerotable.get_CNB_Total(print_sym(alpha_new), phi_new, mach, iota)
-# this function is being called in a couple different places, 2 of which have
-# differing values.
-extra = vel_hat_proj[2]
+modules = {}
+modules.update(aerotable.modules)
+modules.update(atmosphere.modules)
+func = Desym(vars, extra2_, modules=modules)
+ret = func(*vars_)
+
+print("%.18f" % extra_)
+print("%.18f" % ret)
+print()
+print(extra_ - ret)
+
+quit()
+
+aerotable_update_sym = Function("aerotable_update") #type:ignore
+aero_module = {
+        "aerotable_update_sym": aerotable_update,
+        }
+######################
+
+
 
 ##################################################
 # Update Equations
