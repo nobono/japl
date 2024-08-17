7f11db4dafd28bfc6b3ed45138c3a210622ce1e0
david <shincdavid@gmail.com>
2024-08-17 16:35:05 -0400

    n/a

--- tests/Plotter/guitest_PyQtGraphPlotter.py
+++ tests/Plotter/guitest_PyQtGraphPlotter.py
@@ -1,45 +1,7 @@
-# import unittest
-from japl import PyQtGraphPlotter
+from japl.Plotter.PyQtGraphPlotter import PyQtGraphPlotter
 # # from japl import SimObject
 import pyqtgraph as pg
 
-
-# class TestPyQtGraphPlotter_1(unittest.TestCase):
-
-
-#     # def setUp(self) -> None:
-#     #     self.plotter = PyQtGraphPlotter()
-
-
-#     def test_instantiate_setup(self):
-#         plotter = PyQtGraphPlotter()
-#         self.assertTrue(plotter.app)
-
-
-#     def test_instantiate_add_window(self):
-#         plotter = PyQtGraphPlotter()
-#         win = plotter.add_window()
-#         self.assertEqual(len(plotter.wins), 1)
-#         self.assertEqual(len(plotter.shortcuts), 1)
-#         self.assertIsInstance(win, pg.GraphicsLayoutWidget)
-
-
-#     def test_instantiate_add_plot(self):
-#         plotter = PyQtGraphPlotter()
-#         win = plotter.add_window()
-#         color_code = next(plotter.color_cycle)
-#         plot_item = plotter.add_plot(win,
-#                                      title="test",
-#                                      row=0,
-#                                      col=0,
-#                                      color_code=color_code,
-#                                      size=1,
-#                                      aspect="equal")
-#         self.assertIsInstance(plot_item, pg.PlotDataItem)
-
-
-# if __name__ == '__main__':
-#     unittest.main()
 
 
 class TestPyQtGraphPlotter():
