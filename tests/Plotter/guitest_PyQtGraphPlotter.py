ad79cd3a37c292da6d93a0370f57fbe03f7c0537
david <shincdavid@gmail.com>
2024-08-17 17:10:51 -0400

    n/a

--- tests/Plotter/guitest_PyQtGraphPlotter.py
+++ tests/Plotter/guitest_PyQtGraphPlotter.py
@@ -12,26 +12,26 @@ class TestPyQtGraphPlotter():
         assert plotter.app
 
 
-    def test_instantiate_add_window(self):
-        plotter = PyQtGraphPlotter()
-        win = plotter.add_window()
-        assert (len(plotter.wins) == 1)
-        assert (len(plotter.shortcuts) == 1)
-        assert isinstance(win, pg.GraphicsLayoutWidget)
+    # def test_instantiate_add_window(self):
+    #     plotter = PyQtGraphPlotter()
+    #     win = plotter.add_window()
+    #     assert (len(plotter.wins) == 1)
+    #     assert (len(plotter.shortcuts) == 1)
+    #     assert isinstance(win, pg.GraphicsLayoutWidget)
 
 
-    def test_instantiate_add_plot(self):
-        plotter = PyQtGraphPlotter()
-        win = plotter.add_window()
-        color_code = next(plotter.color_cycle)
-        plot_item = plotter.add_plot(win,
-                                     title="test",
-                                     row=0,
-                                     col=0,
-                                     color_code=color_code,
-                                     size=1,
-                                     aspect="equal")
-        assert isinstance(plot_item, pg.PlotDataItem)
+    # def test_instantiate_add_plot(self):
+    #     plotter = PyQtGraphPlotter()
+    #     win = plotter.add_window()
+    #     color_code = next(plotter.color_cycle)
+    #     plot_item = plotter.add_plot(win,
+    #                                  title="test",
+    #                                  row=0,
+    #                                  col=0,
+    #                                  color_code=color_code,
+    #                                  size=1,
+    #                                  aspect="equal")
+    #     assert isinstance(plot_item, pg.PlotDataItem)
 
 
 if __name__ == "__main__":
