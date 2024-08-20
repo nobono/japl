ebf08cf7461981b7a38e16121d5e5c50c79c25da
david <shincdavid@gmail.com>
2024-08-20 12:10:18 -0400

    added PyCodeGen

--- derivation/nav/code_gen.py
+++ derivation/nav/code_gen.py
@@ -105,13 +105,20 @@ class CCodeGenerator:
     def get_ccode(self, expression):
         return ccode(expression, type_aliases={real: float32})
 
+
+    def write_lines(self, string, prefix: str = "", postfix: str = "\n"):
+        for line in string.split('\n'):
+            self.file.write(prefix + line + postfix)
+
+
     def write_subexpressions(self, subexpressions):
         write_string = ""
         for item in subexpressions:
             write_string = write_string + str(item[0]) + " = " + self.get_ccode(item[1]) + ";\n"  # type:ignore
 
         write_string = write_string + "\n\n"
-        self.file.write(write_string)
+        # self.file.write(write_string)
+        self.write_lines(write_string, prefix="\t")
 
     def write_matrix(self,
                      matrix,
@@ -143,6 +150,87 @@ class CCodeGenerator:
 
         write_string = write_string + "\n\n"
         self.file.write(write_string)
+
+
+    def write_function_definition(self, name, args):
+        arg_names = ", ".join(str(i) for i in args)
+        string = f"def {name}({arg_names}):\n\n"
+        self.file.write(string)
+
+
+    def close(self):
+        self.file.close()
+
+
+
+class PyCodeGenerator:
+    def __init__(self, file_name):
+        self.file_name = file_name
+        self.file = open(self.file_name, 'w')
+
+    def print_string(self, string):
+        self.file.write("# " + string + "\n")
+
+    def get_ccode(self, expression):
+        return ccode(expression, type_aliases={real: float32})
+
+
+    def write_lines(self, string, prefix: str = "", postfix: str = "\n"):
+        for line in string.split('\n'):
+            self.file.write(prefix + line + postfix)
+
+
+    def write_subexpressions(self, subexpressions):
+        write_string = ""
+        for item in subexpressions:
+            write_string = write_string + str(item[0]) + " = " + self.get_ccode(item[1]) + ";\n"  # type:ignore
+
+        write_string = write_string + "\n\n"
+        # self.file.write(write_string)
+        self.write_lines(write_string, prefix="\t")
+
+    def write_matrix(self,
+                     matrix,
+                     variable_name,
+                     is_symmetric=False,
+                     pre_bracket="[",
+                     post_bracket="]",
+                     separator="]["):
+        write_string = ""
+        if matrix.shape[0] * matrix.shape[1] == 1:
+            write_string = write_string + variable_name + " = " + self.get_ccode(matrix[0]) + ";\n"
+        elif matrix.shape[0] == 1 or matrix.shape[1] == 1:
+            for i in range(0, len(matrix)):
+                write_string = write_string + variable_name +\
+                               pre_bracket +\
+                               str(i + 1) +\
+                               post_bracket + " = " +\
+                               self.get_ccode(matrix[i]) + ";\n"
+
+        else:
+            for j in range(0, matrix.shape[1]):
+                for i in range(0, matrix.shape[0]):
+                    if j >= i or not is_symmetric:
+                        write_string = write_string + variable_name +\
+                                       pre_bracket +\
+                                       str(i + 1) + separator + str(j + 1) +\
+                                       post_bracket + " = " +\
+                                       self.get_ccode(matrix[i, j]) + ";\n"
+
+        write_string = write_string + "\n\n"
+        # self.file.write(write_string)
+        self.write_lines(write_string, prefix="\t")
+
+
+    def write_function_definition(self, name, args):
+        arg_names = ", ".join(str(i) for i in args)
+        string = f"def {name}({arg_names}):\n\n"
+        self.file.write(string)
+
+
+    def write_function_returns(self, returns):
+        return_names = ", ".join(str(i) for i in returns)
+        self.file.write(f"\treturn ({', '.join(return_names)})")
 
 
     def close(self):
