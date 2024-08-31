190395edb938e7922aa38c2e45e5f394c78f6f38
david <shincdavid@gmail.com>
2024-08-30 22:37:01 -0400

    still working on this (MMD)

--- japl/Library/Vehicles/MissileGenericMMD.py
+++ japl/Library/Vehicles/MissileGenericMMD.py
@@ -1,3 +1,4 @@
+import dill
 import os
 import numpy as np
 from sympy import Matrix, Symbol, symbols
@@ -22,6 +23,7 @@ from japl.Math.Rotation import eci_to_ecef
 from japl.Math.Rotation import eci_to_enu
 from japl.Math.RotationSymbolic import ecef_to_lla_sym
 from japl.Library.Earth.Earth import Earth
+from japl import PyQtGraphPlotter
 
 DIR = os.path.dirname(__file__)
 np.set_printoptions(suppress=True, precision=8)
@@ -57,8 +59,8 @@ aerotable = AeroTableSymbolic(DIR + "/../../../aeromodel/cms_sr_stage1aero.mat",
 # Momentless Missile Dynamics Model
 ##################################################
 
-t = symbols("t")
-dt = symbols("dt")
+t = Symbol("t")
+dt = Symbol("dt")
 
 omega_e = Earth.omega
 
@@ -100,8 +102,11 @@ q_m = Matrix([q_0, q_1, q_2, q_3])
 r_i_m = Matrix([pos_i_x, pos_i_y, pos_i_z])  # eci position
 v_i_m = Matrix([vel_i_x, vel_i_y, vel_i_z])  # eci velocity
 a_b_m = Matrix([acc_b_x, acc_b_y, acc_b_z])  # body acceleration
+a_i_m = Matrix(symbols("acc_i_x, acc_i_y, acc_i_z"))  # eci acceleration
 
 mass = Symbol("mass", real=True)
+
+mach = Function("mach", real=True)(t)  # type:ignore
 
 C_s = Symbol("C_s", real=True)  # speed of sound
 
@@ -132,7 +137,7 @@ eci_to_enu_map = Function("eci_to_enu_map")
 
 # (1)
 C_eci_to_ecef = Matrix([
-    [cos(omega_e * t), sin(omega_e * t), 0],
+    [cos(omega_e * t), sin(omega_e * t), 0],   # type:ignore
     [-sin(omega_e * t), cos(omega_e * t), 0],  # type:ignore
     [0, 0, 1]])
 
@@ -140,16 +145,12 @@ C_eci_to_ecef = Matrix([
 C_body_to_eci = Matrix([
     [1 - 2*(q_2**2 + q_3**2), 2*(q_1*q_2 + q_0*q_3) , 2*(q_1*q_3 - q_0*q_2)],   # type:ignore # noqa
     [2*(q_1*q_2 - q_0*q_3) , 1 - 2*(q_1**2 + q_3**2), 2*(q_2*q_3 + q_0*q_1)],   # type:ignore # noqa
-    [2*(q_1*q_3 + q_0*q_2) , 2*(q_2*q_3 - q_0*q_1), 1 - 2*(q_1**2 + q_2**2)]])  # type:ignore # noqa
+    [2*(q_1*q_3 + q_0*q_2) , 2*(q_2*q_3 - q_0*q_1), 1 - 2*(q_1**2 + q_2**2)]]).T  # type:ignore # noqa
 
-# (6)
-a_b_m = ((f_b_A + f_b_T) / mass) + g_b_m
+# (14)
+C_body_to_ecef = C_eci_to_ecef * C_body_to_eci
 
-# (2)
-r_i_m_dot = v_i_m
-
-# (3)
-v_i_m_dot = C_body_to_eci * a_b_m
+##################################################
 
 # (7) Earth-relative position vector
 r_e_m = C_eci_to_ecef * r_i_m
@@ -164,6 +165,8 @@ omega_skew_ie = Matrix([
 # (8) Earth-relative velocity vector
 v_e_e = C_eci_to_ecef * v_i_m - omega_skew_ie * r_e_m
 
+##################################################
+
 # (12)
 V = v_e_e.norm()
 
@@ -176,12 +179,106 @@ M = V / C_s
 rho = 1.293  # kg * m^-3
 q_bar = 0.5 * rho * V**2
 
-# (14)
-C_body_to_ecef = C_eci_to_ecef * C_body_to_eci
+##################################################
+# invert aerodynamics
+##################################################
+
+# (6)
+g_i_m = Matrix([-9.81, 0, 0])
+g_b_m = C_body_to_eci.T * g_i_m
+a_b_m = ((f_b_A + f_b_T) / mass) + g_b_m
 
 # (13) Earth-relative acceleration vector
 a_e_e = C_body_to_ecef * a_b_m - (2 * omega_skew_ie * v_e_e)\
         - (omega_skew_ie * omega_skew_ie * r_e_m)
+
+# (29)
+C_ecef_to_body = C_body_to_ecef.T
+a_b_e = C_ecef_to_body * a_e_e
+
+##################################################
+
+# (32) (33) (34)
+u_hat = (1 + tan(alpha)**2 + tan(beta)**2)  # type:ignore
+v_hat = u_hat * tan(beta)
+w_hat = u_hat * tan(alpha)
+
+# (35) (36) (37)
+u_hat_dot = -(1 + tan(alpha)**2 + tan(beta)**2)**(-3 / 2) * ((alpha_dot * tan(alpha) * sec(alpha)**2) + (beta_dot * tan(beta) * sec(beta)**2)) # type:ignore # noqa
+v_hat_dot = u_hat_dot * tan(beta) + u_hat * beta_dot * sec(beta)**2  # type:ignore
+w_hat_dot = u_hat_dot * tan(alpha) + u_hat * alpha_dot * sec(alpha)**2  # type:ignore
+
+# (30)
+v_b_e_hat = Matrix([u_hat, v_hat, w_hat])
+
+# (31)
+v_b_e_hat_dot = Matrix([u_hat_dot, v_hat_dot, w_hat_dot])
+
+##################################################
+
+# p, q, r = symbols("p, q, r", real=True)  # angular velocities (roll, pitch, yaw)
+p = Function("p", real=True)(t)  # type:ignore
+q = Function("q", real=True)(t)  # type:ignore
+r = Function("r", real=True)(t)  # type:ignore
+
+###############################
+# NOTE: Earth-relative velocity
+# must be derived as a function
+# of MMD states:
+#   - alpha
+#   - beta
+#   - alpha_dot
+#   - beta_dot
+
+# (39)
+v_b_e = V * v_b_e_hat
+u = v_b_e[0]
+v = v_b_e[1]
+w = v_b_e[2]
+
+# (38)
+V_dot = a_b_e.T * v_b_e_hat
+
+# (40)
+v_b_e_dot = V_dot[0] * v_b_e_hat + V * v_b_e_hat_dot
+u_dot = v_b_e_dot[0]
+v_dot = v_b_e_dot[1]
+w_dot = v_b_e_dot[2]
+
+# (41) C_ij are the array elements of C^b_e
+C_11 = C_body_to_ecef.T[0, 0]
+C_12 = C_body_to_ecef.T[0, 1]
+C_13 = C_body_to_ecef.T[0, 2]
+C_21 = C_body_to_ecef.T[1, 0]
+C_22 = C_body_to_ecef.T[1, 1]
+C_23 = C_body_to_ecef.T[1, 2]
+C_31 = C_body_to_ecef.T[2, 0]
+C_32 = C_body_to_ecef.T[2, 1]
+C_33 = C_body_to_ecef.T[2, 2]
+
+# (44)
+C_1 = (C_11 * C_32 - C_12 * C_31) * u + (C_21 * C_32 - C_22 * C_31) * v
+C_2 = (C_11 * C_22 - C_12 * C_21) * u + (C_22 * C_31 - C_21 * C_32) * w
+
+# (42) (43)
+q_new = (w_dot - a_b_e[2] + p * v - omega_e * C_1) / u
+r_new = (a_b_e[1] - v_dot + p * w + omega_e * C_2) / u
+
+##################################################
+
+# (26)
+# TODO
+# TODO
+# TODO q -> q_new?
+omega_b_ib = Matrix([p, q_new, r_new])
+
+##################################################
+
+# (2)
+r_i_m_dot = v_i_m
+
+# (3)
+v_i_m_dot = C_body_to_eci * a_b_m
 
 ##################################################
 # 2.2 MMD Autopilot Transfer Functions
@@ -220,10 +317,10 @@ beta_state_dot = A_beta * beta_state + B_beta * beta_c
 ############
 
 # (18) Skid-to_Turn (STT) roll angular velocity
-p, q, r = symbols("p, q, r", real=True)  # angular velocities (roll, pitch, yaw)
 K_phi = Symbol("K_phi", real=True)       # roll gain
 omega_p = Symbol("omega_p", real=True)   # natural frequency (roll)
 phi_c = Symbol("phi_c", real=True)       # roll angle command
+T_r = Symbol("T_r", real=True)           # roll autopilot time constant
 
 # (19) Pseudo-roll angle
 phi_hat, phi_hat_dot = symbols("phi_hat, phi_hat_dot", real=True)
@@ -234,7 +331,6 @@ C_eci_to_body = C_body_to_eci.T
 phi = atan2(C_eci_to_body[1, 2], C_eci_to_body[2, 2])
 
 # (24)
-T_r = Symbol("T_r", real=True)  # roll autopilot time constant
 phi_hat = (1 / T_r) * (phi_c - phi)  # type:ignore
 
 # (22)
@@ -250,9 +346,6 @@ p_dot = omega_p * (p_c - p)
 # 2.3 Quaternion State Derivatives
 ##################################################
 
-# (26)
-omega_b_ib = Matrix([p, q, r])
-
 # (25)
 Sq = Matrix([[-q_1, -q_2, -q_3],    # type:ignore
              [q_0, -q_3, q_2],      # type:ignore
@@ -264,80 +357,20 @@ q_m_dot = 0.5 * Sq * omega_b_ib
 # 2.4 Computation of the Missile Angular Velocity
 ##################################################
 
-###############################
-# NOTE: Earth-relative velocity
-# must be derived as a function
-# of MMD states:
-#   - alpha
-#   - beta
-#   - alpha_dot
-#   - beta_dot
-
-# (29)
-C_ecef_to_body = C_body_to_ecef.T
-a_b_e = C_ecef_to_body * a_e_e
-
-# (32) (33) (34)
-u_hat = (1 + tan(alpha)**2 + tan(beta)**2)  # type:ignore
-v_hat = u_hat * tan(beta)
-w_hat = u_hat * tan(alpha)
-
-# (30)
-v_b_e_hat = Matrix([u_hat, v_hat, w_hat])
-
-# (35) (36) (37)
-u_hat_dot = -(1 + tan(alpha)**2 + tan(beta)**2)**(-3 / 2) * ((alpha_dot * tan(alpha) * sec(alpha)**2) + (beta_dot * tan(beta) * sec(beta)**2)) # type:ignore # noqa
-v_hat_dot = u_hat_dot * tan(beta) + u_hat * beta_dot * sec(beta)**2  # type:ignore
-w_hat_dot = u_hat_dot * tan(alpha) + u_hat * alpha_dot * sec(alpha)**2  # type:ignore
-
-# (31)
-v_b_e_hat_dot = Matrix([u_hat_dot, v_hat_dot, w_hat_dot])
-
-# (39)
-v_b_e = V * v_b_e_hat
-u = v_b_e[0]
-v = v_b_e[1]
-w = v_b_e[2]
-
-# (38)
-V_dot = a_b_e.T * v_b_e_hat
-
-# (40)
-v_b_e_dot = V_dot[0] * v_b_e_hat + V * v_b_e_hat_dot
-u_dot = v_b_e_dot[0]
-v_dot = v_b_e_dot[1]
-w_dot = v_b_e_dot[2]
 
 ###############################
 
 # (28)
 omega_skew_ib = Matrix([
-    [0, -r, q],
-    [r, 0, -q],
-    [-q, p, 0]
+    [0, -r, q],  # type:ignore
+    [r, 0, -q],  # type:ignore
+    [-q, p, 0]   # type:ignore
     ])
 
 # (27)
-a_b_e = v_b_e_dot + (omega_skew_ib - C_ecef_to_body * omega_skew_ie * C_body_to_ecef) * v_b_e
+# TODO: include this or not?
+# a_b_e = v_b_e_dot + (omega_skew_ib - C_ecef_to_body * omega_skew_ie * C_body_to_ecef) * v_b_e
 
-# (41) C_ij are the array elements of C^b_e
-C_11 = C_body_to_ecef.T[0, 0]
-C_12 = C_body_to_ecef.T[0, 1]
-C_13 = C_body_to_ecef.T[0, 2]
-C_21 = C_body_to_ecef.T[1, 0]
-C_22 = C_body_to_ecef.T[1, 1]
-C_23 = C_body_to_ecef.T[1, 2]
-C_31 = C_body_to_ecef.T[2, 0]
-C_32 = C_body_to_ecef.T[2, 1]
-C_33 = C_body_to_ecef.T[2, 2]
-
-# (44)
-C_1 = (C_11 * C_32 - C_12 * C_31) * u + (C_21 * C_32 - C_22 * C_31) * v
-C_2 = (C_11 * C_22 - C_12 * C_21) * u + (C_22 * C_31 - C_21 * C_32) * w
-
-# (42) (43)
-q_new = (w_dot - a_b_e[2] + p * v - omega_e * C_1) / u
-r_new = (a_b_e[1] - v_dot + p * w + omega_e * C_2) / u
 
 # (46) NOTE: this is for a BTT missile
 
@@ -361,6 +394,9 @@ a_c_new = sqrt(a_c_y**2 + a_c_z**2)
 
 ###########################################
 r_enu_m = Matrix(symbols("r_e, r_n, r_u"))
+v_enu_m = Matrix(symbols("v_e, v_n, v_u"))
+a_enu_m = Matrix(symbols("a_e, a_n, a_u"))
+
 eci0 = Matrix([Earth.radius_equatorial, 0, 0])
 ecef0 = C_eci_to_ecef * eci0
 lla0 = ecef_to_lla_sym(ecef0)
@@ -372,11 +408,14 @@ C_ecef_to_enu = Matrix([
     [cos(lat) * cos(lon), cos(lat) * sin(lon), sin(lat)]  # type:ignore
     ])
 
+C_eci_to_enu = C_eci_to_ecef * C_ecef_to_enu
+
 r_ecef_m = C_eci_to_ecef * r_i_m
 r_enu_m_new = C_ecef_to_enu * (r_ecef_m - ecef0)
 
-# r_enu_new = eci_to_enu_sym(r_i_m, eci0, t)
-# v_enu_new = eci_to_enu_sym(v_i_m, eci0, t)
+v_enu_m_new = C_eci_to_enu * v_i_m
+a_enu_m_new = C_body_to_eci * C_eci_to_enu * a_b_m
+
 
 thrust = 50_000.0
 # mass = 300.0
@@ -410,7 +449,7 @@ beta_c_out = atan2(tan(alpha_total), sin(phi_Ac))
 ##################################################
 # Differential Definitions
 ##################################################
-alt = (C_eci_to_body * r_i_m)[2]
+alt = r_enu_m_new[2]
 # gacc = atmosphere.grav_accel(alt)  # type:ignore
 gacc = -9.81
 g_b_m_new = (r_i_m / r_i_m.norm()) * gacc
@@ -440,15 +479,20 @@ defs = (
        (q_m.diff(t), q_m_dot_2),
        (r_i_m.diff(t), v_i_m),
        (v_i_m.diff(t), C_body_to_eci * a_b_m),
-       (g_b_m, g_b_m_new),
-       (q, q_new),
-       (r, r_new),
-       (alpha_state, alpha_state_dot),
-       (beta_state, beta_state_dot),
-       (alpha_c, sp.Float(0.0)),
-       (beta_c, sp.Float(0)),
+       (p.diff(t), p_dot),  # type:ignore
+       # (q, q_new),
+       # (r, r_new),
+       (alpha_state.diff(t), alpha_state_dot),
+       (beta_state.diff(t), beta_state_dot),
        (omega_n, sp.Float(50)),
        (zeta, sp.Float(0.7)),
+
+       (K_phi, sp.Float(1.)),
+       (omega_p, sp.Float(20)),
+       (phi_c, sp.Float(0)),
+       (T_r, sp.Float(0.5)),
+       (C_s, sp.Float(343)),
+
        )
 
 ##################################################
@@ -466,6 +510,11 @@ defs = (
 # - Any relationship can be defined in the definition
 #   tuple above.
 # ------------------------------------------------
+acc_b_x = Symbol("acc_b_x", real=True)  # type:ignore
+acc_b_y = Symbol("acc_b_y", real=True)  # type:ignore
+acc_b_z = Symbol("acc_b_z", real=True)  # type:ignore
+st_a_b_m = Matrix([acc_b_x, acc_b_y, acc_b_z])
+
 
 state = Matrix([
     q_m,
@@ -474,15 +523,20 @@ state = Matrix([
     alpha_state,
     beta_state,
     p,
-    q,
-    r,
+    # q,
+    # r,
     mass,
     DirectUpdate(r_enu_m, r_enu_m_new),
-    a_b_m,
+    DirectUpdate(v_enu_m, v_enu_m_new),
+    DirectUpdate(a_enu_m, a_enu_m_new),
+    # DirectUpdate(a_i_m, C_eci_to_ecef.T * a_e_e),
+    DirectUpdate(mach, M),
     ])
 
 input = Matrix([
     # DirectUpdate(f_b_A, force_aero),
+    alpha_c,
+    beta_c,
     f_b_A,
     f_b_T,
     ])
@@ -513,20 +567,28 @@ dynamics = state.diff(t)
 #                               Matrix(dynamics),
 #                               defs)
 
-modules = {"ecef_to_lla_map": ecef_to_lla,
-           "ecef_to_enu_map": ecef_to_enu,
-           "ecef_to_eci_map": ecef_to_eci,
-           "eci_to_ecef_map": eci_to_ecef,
-           "eci_to_enu_map": eci_to_enu}
+custom_modules = {"ecef_to_lla_map": ecef_to_lla,
+                  "ecef_to_enu_map": ecef_to_enu,
+                  "ecef_to_eci_map": ecef_to_eci,
+                  "eci_to_ecef_map": eci_to_ecef,
+                  "eci_to_enu_map": eci_to_enu}
+
+modules = [atmosphere.modules,
+           aerotable.modules,
+           custom_modules]
 
 model = MissileGenericMMD.from_expression(dt,
                                           state,
                                           input,
                                           dynamics,
-                                          modules=[atmosphere.modules,
-                                                   aerotable.modules,
-                                                   modules],
+                                          modules=modules,
                                           definitions=defs)
+
+
+print("saving model...")
+with open(f"{DIR}/mmd.pickle", 'wb') as f:
+    dill.dump(model, f)
+quit()
 
 
 simobj = SimObject(model)
@@ -538,12 +600,13 @@ beta_state_0 = [0, 0]
 pqr0 = [0, 0, 0]
 mass0 = 300
 r_enu0 = [0, 0, 0]
+a0 = [0, 0, 0]
 
 # simobj.init_state([q0, r0, v0, alpha0, beta0, p0])
 simobj.init_state([q0, r0, v0, alpha_state_0, beta_state_0, pqr0, mass0,
-                   r_enu0])
+                   r_enu0, a0])
 
-sim = Sim(t_span=[0, 5], dt=0.01, simobjs=[simobj],
+sim = Sim(t_span=[0, 20], dt=0.01, simobjs=[simobj],
           integrate_method="rk4")
 sim.run()
 
@@ -556,6 +619,19 @@ print("beta_state:", simobj.Y[-1, 12:14])
 print("pqr:", simobj.Y[-1, 14:17])
 print("mass:", simobj.Y[-1, 17])
 print("r_enu:", simobj.Y[-1, 18:21])
+
+# simobj.plot.set_config({
+#     "pos-x": {
+#         "xaxis": 'pos_n',
+#         "yaxis": 'pos_e'
+#         },
+#     })
+
+plotter = PyQtGraphPlotter(frame_rate=30, figsize=[10, 6], aspect="auto")
+# plotter.plot(sim.T, simobj.Y[:, 20])
+plotter.plot(simobj.Y[:, 19], simobj.Y[:, 20])
+plotter.show()
+
 quit()
 
 ################
