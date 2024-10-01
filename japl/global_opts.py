aac42db6721130fe6884be99246ec7e0b4372dd9
david <shincdavid@gmail.com>
2024-10-01 09:00:30 -0400

    refactor

--- japl/global_opts.py
+++ japl/global_opts.py
@@ -2,7 +2,7 @@ import os
 
 
 
-JAPL_HOME_DIR = os.path.join(os.path.dirname(__file__), "../")
+JAPL_HOME_DIR = os.path.abspath(os.path.join(os.path.dirname(__file__), "../"))
 
 __PLOTTING_BACKEND = "pyqtgraph"
 
