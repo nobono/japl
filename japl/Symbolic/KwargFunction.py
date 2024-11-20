b16e1d54e2bdaba9d3bf8662185cf88947ae34fe
david <shincdavid@gmail.com>
2024-11-20 14:16:59 -0500

    completed KwargFunction

--- japl/Symbolic/KwargFunction.py
+++ japl/Symbolic/KwargFunction.py
@@ -1,6 +1,5 @@
+from typing import Any
 from sympy import Function
-from sympy import Mul
-from sympy import Add
 
 
 
@@ -9,14 +8,15 @@ class KwargFunction(Function):
     """This class inherits from sympy.Function and allows
     keyword arguments."""
 
-    kwargs = {}
+    __slots__ = ("name", "kwargs")
 
     @classmethod
     def eval(cls, *args, **kwargs):
         """kwargs are not used in eval by default; augmenting here."""
         return super().eval(*args)
 
-    def __new__(cls, name, **kwargs):
+
+    def __new__(cls, name: str, kwargs: dict = {}):
         # create the object
         args = tuple([v for v in kwargs.values()])
         obj = super().__new__(cls, *args)
@@ -26,34 +26,44 @@ class KwargFunction(Function):
         return obj
 
 
+    def __reduce__(self) -> str | tuple[Any, ...]:
+        """defines serialization of class object"""
+        return (self.__class__, (self.name, self.kwargs), self.__getstate__())
+
+
     def __repr__(self) -> str:
         return self.__str__()
 
 
     def __str__(self) -> str:
-        _kwargs = ",".join([f"{k}={v}" for k, v in self.kwargs.items()])
+        _kwargs = ", ".join([f"{k}={v}" for k, v in self.kwargs.items()])
         return f"{self.name}({_kwargs})"
 
 
     def _pythoncode(self, *args, **kwargs):
+        """string representation of object when using sympy.pycode()
+        for python code generation"""
         return self.__str__()
 
 
     def _ccode(self, *args, **kwargs):
+        """string representation of object when using sympy.ccode()
+        for c-code generate"""
+        _kwargs = []
+        for k, v in self.kwargs.items():
+            _kwargs += [f"py::kw(\"{k}\"_a={v})"]
+        _kwargs = ", ".join(_kwargs)
+        return f"{self.name}({_kwargs})"
+
+
+    def _sympystr(self, printer):
+        """string representation of object used in sympy string formatting."""
         return self.__str__()
 
 
-    def __call__(self, **kwargs):
+    def __call__(self, *args, **kwargs):
         args = tuple([v for v in kwargs.values()])
         obj = super().__new__(self.__class__, *args)
         obj.kwargs = kwargs
         obj.name = self.name
         return obj
-
-
-    def __mul__(self, other):
-        return Mul(self, other)
-
-
-    def __add__(self, other):
-        return Add(self, other)
--- tests/Symbolic/test_KwargFunction.py
+++ tests/Symbolic/test_KwargFunction.py
@@ -3,6 +3,8 @@ from sympy import Symbol
 from japl.Symbolic.KwargFunction import KwargFunction
 from sympy import pycode
 from sympy import ccode
+import dill as pickle
+import io
 
 
 
@@ -19,15 +21,17 @@ class TestKwargFunction(unittest.TestCase):
         self.assertEqual(str(func), "func()")
         self.assertEqual(func.args, ())
         self.assertEqual(func.kwargs, {})
+        self.assertEqual(func.name, "func")
 
 
     def test_case2(self):
         """number args"""
         func = KwargFunction('func')
         func = func(a=1, b=2)
-        self.assertEqual(str(func), "func(a=1,b=2)")
+        self.assertEqual(str(func), "func(a=1, b=2)")
         self.assertEqual(func.args, (1, 2))
         self.assertEqual(func.kwargs, {'a': 1, 'b': 2})
+        self.assertEqual(func.name, "func")
 
 
     def test_case3(self):
@@ -35,9 +39,10 @@ class TestKwargFunction(unittest.TestCase):
         b = Symbol('b')
         func = KwargFunction('func')
         func = func(a=1, b=b)
-        self.assertEqual(str(func), "func(a=1,b=b)")
+        self.assertEqual(str(func), "func(a=1, b=b)")
         self.assertEqual(func.args, (1, b))
         self.assertEqual(func.kwargs, {'a': 1, 'b': b})
+        self.assertEqual(func.name, "func")
 
 
     def test_pycode_case1(self):
@@ -45,9 +50,10 @@ class TestKwargFunction(unittest.TestCase):
         b = Symbol('b')
         func = KwargFunction('func')
         func = func(a=1, b=b)
-        self.assertEqual(pycode(func), "func(a=1,b=b)")
+        self.assertEqual(pycode(func), "func(a=1, b=b)")
         self.assertEqual(func.args, (1, b))
         self.assertEqual(func.kwargs, {'a': 1, 'b': b})
+        self.assertEqual(func.name, "func")
 
 
     def test_ccode_case1(self):
@@ -55,9 +61,36 @@ class TestKwargFunction(unittest.TestCase):
         b = Symbol('b')
         func = KwargFunction('func')
         func = func(a=1, b=b)
-        self.assertEqual(ccode(func), r"func(a=1,b=b)")
+        self.assertEqual(ccode(func), "func(py::kw(\"a\"_a=1), py::kw(\"b\"_a=b))")
         self.assertEqual(func.args, (1, b))
         self.assertEqual(func.kwargs, {'a': 1, 'b': b})
+        self.assertEqual(func.name, "func")
+
+
+    def test_add_case1(self):
+        """addition"""
+        b = Symbol('b')
+        func = KwargFunction('func')
+        func = func(a=1, b=b)
+        ret = func + 1
+        self.assertEqual(str(ret), "func(a=1, b=b) + 1")
+        self.assertEqual(ret.args, (1, func))
+        self.assertEqual(ret.args[1].args, (1, b))
+        self.assertEqual(ret.args[1].kwargs, {'a': 1, 'b': b})
+
+
+    def test_pickle_case1(self):
+        """symbol args"""
+        b = Symbol('b')
+        func = KwargFunction('func')
+        func = func(a=1, b=b)
+        file = io.BytesIO()
+        pickle.dump(func, file)
+        file.seek(0)
+        loaded_data = pickle.load(file)
+        self.assertEqual(loaded_data.name, "func")
+        self.assertEqual(loaded_data.args, (1, b))
+        self.assertEqual(loaded_data.kwargs, {'a': 1, 'b': b})
 
 
 if __name__ == '__main__':
