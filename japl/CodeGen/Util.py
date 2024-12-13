a9846b57da71eac81db17588456c17cc2e92b508
david <shincdavid@gmail.com>
2024-12-13 01:03:27 -0500

    n/a

--- japl/CodeGen/Util.py
+++ japl/CodeGen/Util.py
@@ -1,30 +1,6 @@
 import os
 import shutil
-from sympy.codegen.ast import numbered_symbols
-from japl.CodeGen.Globals import _STD_DUMMY_NAME
 
-
-
-__numbered_dummy_symbol_gen = numbered_symbols(prefix=_STD_DUMMY_NAME)
-
-
-def get_dummy_symbol():
-    return next(__numbered_dummy_symbol_gen)
-
-
-def reset_dummy_symbol_gen():
-    global __numbered_dummy_symbol_gen
-    __numbered_dummy_symbol_gen = numbered_symbols(prefix=_STD_DUMMY_NAME)
-
-
-# def ccode(expr, **kwargs):
-#     printer = CCodeGenPrinter()
-#     return printer.doprint(expr, **kwargs)
-
-
-# def pycode(expr, **kwargs):
-#     printer = PyCodeGenPrinter()
-#     return printer.doprint(expr, **kwargs)
 
 
 def copy_dir(source_dir, target_dir) -> None:
