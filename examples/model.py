2e7f2077c051b12ff5417dee1b7dbf99a38fdf77
david <shincdavid@gmail.com>
2024-12-17 14:46:16 -0500

    n/a

--- examples/model.py
+++ examples/model.py
@@ -10,17 +10,17 @@ from japl.CodeGen import ccode, pycode
 t = symbols("t")
 dt = symbols("dt")
 
-pos_x = Symbol("pos_x", real=True)
-pos_y = Symbol("pos_y", real=True)
-pos_z = Symbol("pos_z", real=True)
+pos_x = Symbol("pos_x")
+pos_y = Symbol("pos_y")
+pos_z = Symbol("pos_z")
 
-vel_x = Symbol("vel_x", real=True)
-vel_y = Symbol("vel_y", real=True)
-vel_z = Symbol("vel_z", real=True)
+vel_x = Symbol("vel_x")
+vel_y = Symbol("vel_y")
+vel_z = Symbol("vel_z")
 
-acc_x = Symbol("acc_x", real=True)
-acc_y = Symbol("acc_y", real=True)
-acc_z = Symbol("acc_z", real=True)
+acc_x = Symbol("acc_x")
+acc_y = Symbol("acc_y")
+acc_z = Symbol("acc_z")
 
 pos = Matrix([pos_x, pos_y, pos_z])
 vel = Matrix([vel_x, vel_y, vel_z])
