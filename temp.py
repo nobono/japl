e3b06be110d7f59d8ae51d81c8c5732efa569089
david <shincdavid@gmail.com>
2024-12-20 16:25:30 -0500

    n/a

--- temp.py
+++ temp.py
@@ -1,18 +1,22 @@
-# from sympy import symbols
-# from sympy import Matrix
-# from sympy import true, false
-# from sympy import cse
-# from sympy import MatrixSymbol
-# from sympy import Symbol
-# from sympy import Function
-# from sympy.codegen.ast import CodeBlock, Element
-# from sympy.codegen.ast import Type
-# from sympy.codegen.ast import String
-# from sympy.matrices.expressions.matexpr import MatrixElement
-# from sympy.printing.c import Assignment, Declaration
-# from japl.CodeGen import pycode
-# from japl.CodeGen import ccode
-# from japl.CodeGen import Builder
+import numpy as np
+from typing import Any, Callable
+from sympy import Basic
+from sympy import symbols
+from sympy import Matrix
+from sympy import true, false
+from sympy import cse
+from sympy import MatrixSymbol
+from sympy import Symbol
+from sympy import Function
+from sympy import Expr
+from sympy.codegen.ast import CodeBlock, Element
+from sympy.codegen.ast import Type
+from sympy.codegen.ast import String
+from sympy.codegen.ast import Token
+from sympy.codegen.ast import Tuple
+from japl.CodeGen import pycode
+from japl.CodeGen import ccode
+from japl.CodeGen import Builder
 # from japl.CodeGen import FileBuilder
 # from japl.CodeGen import CFileBuilder
 # from japl.CodeGen import ModuleBuilder
@@ -25,28 +29,25 @@
 # from japl.CodeGen.Util import optimize_expression
 # from japl.Library.Earth.Earth import Earth
 # from sympy import sin, cos
-from japl import Model
+# from japl.CodeGen.Ast import JaplClass
+from japl import SimObject
+from japl.CodeGen.Ast import CodeGenFunctionCall
+from japl.CodeGen.Ast import Kwargs
+from japl.CodeGen.JaplFunction import JaplFunction
+from japl.Util.Util import iter_type_check
 
+from pprint import pprint
+import mmd
+m = mmd.Model()
+print(m.state_dim)
+s = mmd.SimObject()
+print(s.r_i_x)
+# print(mmd.simobj().model.dynamics)
 
-
-# t = symbols("t")
-# dt = symbols("dt")
-
-# gacc = Symbol("gacc", real=True)
-# omega_e = Earth.omega
-# x = Function("x", real=True)(t)
-# y = Function("y", real=True)(t)
-# z = Function("z", real=True)(t)
-# vx = Function("vx", real=True)(t)
-# vy = Function("vy", real=True)(t)
-# vz = Function("vz", real=True)(t)
-# pos = Matrix([x, y, z])  # eci position
-# vel = Matrix([vx, vy, vz])
-
-# q0 = Function("q0", real=True)(t)
-# q1 = Function("q1", real=True)(t)
-# q2 = Function("q2", real=True)(t)
-# q3 = Function("q3", real=True)(t)
-
-from mtest import model as mod
-# from japl import Model
+# pprint(m.input_vars)
+# so = SimObject(m)
+# so.init_state([0.] * so.state_dim)
+# T = np.zeros((10,))
+# so._init_data_array(T)
+# so._pre_sim_checks()
+# print(mmd.simobj().model.dynamics(0., [2.] * 80, [0.] * 80, [0.] * 80, 0.1))
