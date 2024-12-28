13debe753f564df09bd06941d2ef595a1f2aad75
david <shincdavid@gmail.com>
2024-12-28 01:30:06 -0500

    cpp_datatable unittests

--- tests/cpp/test_CppDataTable.py
+++ tests/cpp/test_CppDataTable.py
@@ -1,6 +1,7 @@
 import unittest
 import numpy as np
 from japl.DataTable.DataTable import DataTable
+import linterp
 import model
 import atmosphere
 import aerotable
@@ -21,34 +22,42 @@ class TestCppDataTable(unittest.TestCase):
                      "mach": np.array([0., 1, 2])}
 
 
-    def test_datatable_dict(self):
+    def test_datatable_members(self):
+        table = datatable.DataTable(self.data, self.axes)
+        self.assertListEqual(list(table.axes.keys()), ["alpha", "mach"])
+        self.assertIsInstance(table.interp, linterp.Interp2d)
+        self.assertTrue((table.interp._data == self.data).all())
+
+
+    def test_datatable_call(self):
         """test call datable with dict"""
         table = datatable.DataTable(self.data, self.axes)
         ret = table(dict(alpha=1., mach=1.))
         self.assertListEqual(ret.tolist(), [5.])
 
-
-    def test_datatable_kwargs(self):
         """test call datable with kwargs"""
         table = datatable.DataTable(self.data, self.axes)
         ret = table(alpha=1., mach=1.)
         self.assertListEqual(ret.tolist(), [5.])
 
-
-    def test_datatable_iter(self):
-        """test call datable with tuple"""
+        """test call datable with iterable"""
         table = datatable.DataTable(self.data, self.axes)
         ret = table([[1., 1.]])
         self.assertListEqual(ret.tolist(), [5.])
 
-
-    def test_aero_table(self):
-        """test aerotable table usage"""
+        """test call datatable with list of points"""
         table = datatable.DataTable(self.data, self.axes)
-        table2 = datatable.DataTable(self.data * 2, self.axes)
-        aero = aerotable.AeroTable(CA=table, CNB=table2)
-        # self.assertListEqual(aero.CA(alpha=1, mach=1).tolist(), [5.])
-        # self.assertListEqual(aero.CNB(alpha=1, mach=1).tolist(), [10.])
+        ret = table([[0, 0], [1, 1], [2, 2]])
+        self.assertListEqual(ret.tolist(), [1, 5, 9])
+
+
+    # def test_aero_table(self):
+    #     """test aerotable table usage"""
+    #     table = datatable.DataTable(self.data, self.axes)
+    #     table2 = datatable.DataTable(self.data * 2, self.axes)
+    #     aero = aerotable.AeroTable(CA=table, CNB=table2)
+    #     # self.assertListEqual(aero.CA(alpha=1, mach=1).tolist(), [5.])
+    #     # self.assertListEqual(aero.CNB(alpha=1, mach=1).tolist(), [10.])
 
 
     # def test_aero_float_case_1(self):
